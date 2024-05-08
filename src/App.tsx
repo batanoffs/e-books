e1106925ceb2edf75c2a94c32b9de1ad01753623
batanoffs <batanoff.s@protonmail.com>
2024-05-08 21:15:52 +0300

    added basic layout and card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
-import { useState } from 'react'
-import './input.css'
+import { BookCard } from './components/card'
+import './index.css'
+import { Button } from '@mui/material'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <nav className="border-solid  px-40 border border-sky-500">
+                <div className="flex justify-between px-4 py-5">
+                    <div className="">
+                        <img src="" alt="bookstore logo" />
+                    </div>
+                    <div className="flex gap-5">
+                        <a href="">Tab</a>
+                        <a href="">Tab</a>
+                        <a href="">Tab</a>
+                        <a href="">Tab</a>
+                        <a href="">Tab</a>
+                        <Button variant="outlined">Login</Button>
+                        <Button variant="contained">Register</Button>
+                    </div>
+                </div>{' '}
+            </nav>
+            <div className="grid grid-rows-6  mx-40">
+                <div className="border-solid border border-gray-500"> 01 </div>
+                <div className="border-solid border flex gap-4 border-gray-500 py-8"> 
+                    <BookCard />
+                    <BookCard />
+                    <BookCard />
+                    <BookCard />
+                </div>
+                <div className="border-solid border border-gray-500"> 03 </div>
+                <div className="border-solid border border-gray-500"> 04 </div>
+                <div className="border-solid border border-gray-500"> 05 </div>
+                <div className="border-solid border border-gray-500"> 06 </div>
+            </div>
+        </>
+    )
 }
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  important: "#root",
   theme: {
     screens: {
       sm: '480px',
