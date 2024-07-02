03152a5ecc27b90ed119d681ef8a37a384720072
batanoffs <batanoff.s@protonmail.com>
2024-07-02 19:08:15 +0300

    refactored with new service for register and login

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,20 +1,36 @@
-import { Request, Response } from "express-serve-static-core";
-import { createToken } from "../services/jwt";
+import { Request, Response } from 'express-serve-static-core';
+import { createToken } from '../services/jwt';
+const { validationResult } = require('express-validator');
 
-import User from "../models/User";
+import User from '../models/User';
+import { registerUser } from '../services/user';
 
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
 
 const register = async (req: Request, res: Response) => {
-    const { username, password, role } = req.body;
+    const { email, password, role } = req.body;
 
     try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = new User({ username, password: hashedPassword, role });
-        await newUser.save();
-        res.status(201).json({ message: "User registered successfully" });
+        const validation = validationResult(req);
+
+        if (validation.errors.length) {
+            throw validation.errors;
+        }
+        const user = await registerUser(email, password, role);
+        console.log(user);
+
+        const token = createToken(user);
+
+        res.status(201).json({ message: 'User registered successfully' });
+
+        res.cookie('token', token, { httpOnly: true });
+        if (role === 'admin') {
+            res.redirect('/admin/dashboard');
+        } else {
+            res.redirect('/');
+        }
     } catch (error) {
-        res.status(500).json({ message: "Registration failed", error });
+        res.status(500).json({ message: 'Registration failed', error });
     }
 };
 
@@ -24,25 +40,24 @@ const login = async (req: Request, res: Response) => {
     try {
         const user = await User.findOne({ email });
         if (!user || !(await bcrypt.compare(password, user.password))) {
-            return res.status(401).json({ message: "Invalid credentials" });
-            
+            return res.status(401).json({ message: 'Invalid credentials' });
         }
 
         console.log(user);
 
         const token = createToken(user);
 
-        res.cookie("token", token, { httpOnly: true });
-        res.status(200).json({ message: "Login successful", token });
+        res.cookie('token', token, { httpOnly: true });
+        res.status(200).json({ message: 'Login successful', token });
     } catch (error) {
-        res.status(500).json({ message: "Login failed", error });
+        res.status(500).json({ message: 'Login failed', error });
     }
 };
 
 const logout = (req: Request, res: Response) => {
-    res.clearCookie("token");
-    res.status(200).json({ message: "Logout successful" });
-    res.redirect("/"); // TODO check redirect according to documentation
+    res.clearCookie('token');
+    res.status(200).json({ message: 'Logout successful' });
+    res.redirect('/'); // TODO check redirect according to documentation
 };
 
 export { register, login, logout };
