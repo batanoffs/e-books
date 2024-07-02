2d13d2acee12ccb975749a9822637de62279ce3b
batanoffs <batanoff.s@protonmail.com>
2024-07-02 19:06:20 +0300

    updated visuals for user model

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,30 +1,37 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema, Document } from 'mongoose';
 
-interface IUser extends Document {
+export interface IUser extends Document {
     email: string;
     password: string;
-    role: "user" | "admin";
+    role: 'user' | 'admin';
 }
 
 const UserSchema: Schema = new Schema(
     {
-        email: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
         role: {
             type: String,
             required: true,
-            enum: ["user", "admin"],
-            default: "user",
+            enum: ['user', 'admin'],
+            default: 'user',
         },
     },
     {
         collation: {
-            locale: "en",
+            locale: 'en',
             strength: 2,
         },
     }
 );
 
-const User = mongoose.model<IUser>("User", UserSchema);
+const User = mongoose.model<IUser>('User', UserSchema);
 
 export default User;
