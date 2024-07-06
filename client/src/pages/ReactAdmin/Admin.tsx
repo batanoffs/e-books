da43165bf40814f6dc22d5fe32579924869db41d
batanoffs <batanoff.s@protonmail.com>
2024-07-06 20:55:53 +0300

    fixed routes for admin panel

--- client/src/pages/ReactAdmin/Admin.tsx
+++ client/src/pages/ReactAdmin/Admin.tsx
@@ -1,16 +1,17 @@
 import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
 import restProvider from './dataProvider2';
+import authProvider from './authProvider';
 
 const apiUrl = 'http://localhost:5001/api/admin';
 const dataProvider = restProvider(apiUrl);
 
 const AdminPage = () => (
-    <Admin basename="/admin" dataProvider={dataProvider}>
-        <Resource name="/users" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
-        <Resource name="/orders" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
-        <Resource name="/books" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
-        <Resource name="/items" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
-        <Resource name="/featured" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+    <Admin basename="/admin" dataProvider={dataProvider} authProvider={authProvider}>
+        <Resource name="users" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+        <Resource name="orders" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+        <Resource name="books" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+        <Resource name="items" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+        <Resource name="featured" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
     </Admin>
 );
 
