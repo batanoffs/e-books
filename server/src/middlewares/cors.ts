3e7034e900e22cff746d279876b95539cf307f0b
batanoffs <batanoff.s@protonmail.com>
2024-07-06 20:54:57 +0300

    updated server headers for pagination

--- server/src/middlewares/cors.ts
+++ server/src/middlewares/cors.ts
@@ -5,7 +5,8 @@ const count = '1/1';
 function cors() {
     return function (req: Request, res: Response, next: NextFunction) {
         res.setHeader('Access-Control-Allow-Origin', ALLOWED_ADDRESS);
-        res.setHeader("Access-Control-Expose-Headers", "X-Total-Count");
+        res.setHeader('Access-Control-Expose-Headers', 'X-Total-Count');
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
         res.setHeader('X-Total-Count', count);
 
         if (req.method === 'OPTIONS') {
@@ -19,5 +20,3 @@ function cors() {
 }
 
 export { cors };
-
-
