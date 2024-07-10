7629f7cf2e15212357f0ca71478b7f4e2850f793
batanoffs <batanoff.s@protonmail.com>
2024-07-10 18:10:02 +0300

    moved theme provider to main tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from '@mui/material/styles';
+import themeOptions from './utils/theme';
 
 import App from './App';
 
@@ -7,6 +9,8 @@ const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
     <React.StrictMode>
-        <App />
+        <ThemeProvider theme={themeOptions.whiteTheme}>
+            <App />
+        </ThemeProvider>
     </React.StrictMode>
 );
