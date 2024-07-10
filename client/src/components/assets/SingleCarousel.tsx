f36b7f22f5365acb78892fd41c69ea27f1d9db39
batanoffs <batanoff.s@protonmail.com>
2024-07-10 18:07:19 +0300

    updated single carousel

--- client/src/components/assets/SingleCarousel.tsx
+++ client/src/components/assets/SingleCarousel.tsx
@@ -1,11 +1,11 @@
 import Slider from 'react-slick';
 
-import { Featured } from '../Featured.tsx';
+import { Featured } from './Featured.tsx';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-import styles from './single-carousel.module.css';
+import styles from './single-carousel.module.scss';
 
 interface BookProps {
     books: {
@@ -19,28 +19,45 @@ interface BookProps {
         stock: number;
     }[];
 }
+function SampleNextArrow(props) {
+    const { onClick } = props;
+    return <div className={styles.nextArrow} onClick={onClick} />;
+}
+
+function SamplePrevArrow(props) {
+    const { onClick } = props;
+    return <div className={styles.prevArrow} onClick={onClick} />;
+}
 
 export const SingleCarousel = ({ books }: BookProps) => {
     const settings = {
         dots: true,
         infinite: true,
-        speed: 3000,
+        autoplay: true,
+        speed: 5000,
+        autoplaySpeed: 10000,
+        arrows: true,
+        centerPadding: '60px',
+        pauseOnHover: true,
+        swipeToSlide: true,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />,
+        cssEase: 'linear',
+
         responsive: [
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true,
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
                 },
             },
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    initialSlide: 1,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2,
                 },
             },
             {
@@ -53,12 +70,10 @@ export const SingleCarousel = ({ books }: BookProps) => {
         ],
     };
     return (
-        <div className={styles.carousel}>
-            <Slider {...settings}>
-                {books.map((book) => (
-                    <Featured key={book._id} book={book} styles={styles} />
-                ))}
-            </Slider>
-        </div>
+        <Slider className={styles.carousel} {...settings}>
+            {books.map((book) => (
+                <Featured key={book._id} book={book} styles={styles} />
+            ))}
+        </Slider>
     );
 };
