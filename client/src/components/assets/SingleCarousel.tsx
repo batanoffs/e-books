84ba8e6463f66dbca3f6fafbf42dd4df64476624
batanoffs <batanoff.s@protonmail.com>
2024-07-13 19:40:15 +0300

    updated responsive carousel

--- client/src/components/assets/SingleCarousel.tsx
+++ client/src/components/assets/SingleCarousel.tsx
@@ -48,16 +48,15 @@ export const SingleCarousel = ({ books }: BookProps) => {
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
                 },
             },
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    initialSlide: 2,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
                 },
             },
             {
