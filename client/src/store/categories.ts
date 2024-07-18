169a1c1513b982c6198072a2a7c256dd7c4bd927
batanoffs <batanoff.s@protonmail.com>
2024-07-18 05:32:10 +0300

    updated items store

--- client/src/store/categories.ts
+++ client/src/store/categories.ts
@@ -4,24 +4,24 @@ type CategoriesState = {
     navCategory: string | null;
     bookCategories: string[];
     textbookCategories: string[];
-    itemCategories: string[];
+    stationeriesCategories: string[];
     filters: string[];
     setNavCategory: (category: string | null) => void;
     setFilters: (filters: string[]) => void;
     setBookCategories: (categories: string[]) => void;
     setTextbookCategories: (categories: string[]) => void;
-    setItemCategories: (categories: string[]) => void;
+    setStationeriesCategories: (categories: string[]) => void;
 };
 
 export const useFiltersStore = create<CategoriesState>((set) => ({
     navCategory: 'all',
     bookCategories: [],
     textbookCategories: [],
-    itemCategories: [],
+    stationeriesCategories: [],
     filters: [],
     setBookCategories: (bookCategories: string[]) => set({ bookCategories }),
     setTextbookCategories: (textbookCategories: string[]) => set({ textbookCategories }),
-    setItemCategories: (itemCategories: string[]) => set({ itemCategories }),
+    setStationeriesCategories: (itemCategories: string[]) => set({ itemCategories }),
     setNavCategory: (navCategory: string | null) => set({ navCategory }),
     setFilters: (filters: string[]) => set({ filters }),
 }));
