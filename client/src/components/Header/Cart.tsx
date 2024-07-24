5728b9bbcb72ad66c0c88f5d8b7295c14c9859d3
batanoffs <batanoff.s@protonmail.com>
2024-07-24 12:57:24 +0300

    updated cart button in header

--- client/src/components/Header/Cart.tsx
+++ client/src/components/Header/Cart.tsx
@@ -1,7 +1,7 @@
 import { useTheme } from '@mui/material/styles';
 import { useNavigate } from 'react-router-dom';
 import { useMediaQuery, Button, Badge } from '@mui/material';
-import ShoppingCart from '@mui/icons-material/ShoppingCart';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import useCartStore from '../../store/cart';
 
 const CartButton = () => {
@@ -9,27 +9,23 @@ const CartButton = () => {
     const theme = useTheme();
     const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
-    const handleClick = () => {
+    const handleNavigateToCart = () => {
         navigate('/cart');
     };
 
-    return (
-        <Button
-            variant="contained"
-            color="primary"
-            size={isSmallScreen ? 'small' : 'large'}
-            startIcon={<ShoppingCart />}
-            onClick={handleClick}
-            className="cart-button"
-        >
-            <Badge badgeContent={getCartItemCount()} color="error" />
-        </Button>
-    );
-};
+    const cartItemCount = useCartStore((state) => state.cart.length);
 
-const getCartItemCount = () => {
-    const cart = useCartStore((state) => state.cart);
-    return cart.length;
+    return (
+        <Badge badgeContent={cartItemCount} color="primary">
+            <Button
+                variant="contained"
+                color="primary"
+                size={isSmallScreen ? 'small' : 'large'}
+                startIcon={<ShoppingCartIcon />}
+                onClick={handleNavigateToCart}
+            ></Button>
+        </Badge>
+    );
 };
 
 export default CartButton;
