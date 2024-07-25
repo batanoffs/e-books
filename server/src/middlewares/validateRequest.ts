99b221856f537d159915a932cbbd0642f17daa8b
batanoffs <batanoff.s@protonmail.com>
2024-07-25 22:01:21 +0300

    updated middleware readability

--- server/src/middlewares/validateRequest.ts
+++ server/src/middlewares/validateRequest.ts
@@ -3,8 +3,8 @@ import { validationResult } from 'express-validator';
 
 export default function validateRequest(req: Request, res: Response, next: NextFunction) {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
+
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+
     next();
 }
