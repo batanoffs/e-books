a7a19ac06d341e6813560e901bc39d2b0e64e6bb
batanoffs <batanoff.s@protonmail.com>
2024-07-25 21:58:57 +0300

    updated models

--- server/src/models/Stationery.ts
+++ server/src/models/Stationery.ts
@@ -5,9 +5,19 @@ const StationerySchema: Schema = new Schema({
     title: { type: String, required: true },
     price: { type: Number, required: true },
     description: { type: String, required: true },
-    images: { type: [String], required: true },
+    coverImage: { type: Buffer, required: true }, //TODO update logic for multiple images
+    coverImageType: { type: String, required: true },
     category: { type: String, required: true },
     stock: { type: Number, required: true },
+    createdAt: { type: Date, default: Date.now, required: true },
+});
+
+StationerySchema.virtual('coverImagePath').get(function () {
+    if (this.coverImage != null && this.coverImageType != null) {
+        return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString(
+            'base64'
+        )}`;
+    }
 });
 
 const Stationery = model<IStationerySchema>('Stationery', StationerySchema);
--- server/src/models/Textbook.ts
+++ server/src/models/Textbook.ts
@@ -6,18 +6,27 @@ const TextbookSchema: Schema = new Schema({
     author: { type: String, required: true },
     price: { type: Number, required: true },
     description: { type: String, required: true },
-    images: { type: [String], required: true },
+    coverImage: { type: Buffer, required: true }, //TODO update logic for multiple images
+    coverImageType: { type: String, required: true }, //TODO update logic for multiple images
     stock: { type: Number, required: true },
     category: { type: String, required: true },
     publisher: { type: String, required: false },
     language: { type: String, required: false },
-    yearPublished: { type: Date, required: false },
-    pages: { type: Number, required: false },
+    publishDate: { type: Date, required: false },
+    pageCount: { type: Number, required: false },
     translator: { type: String, required: false },
     dimensions: { type: String, required: false },
-    coverpageType: { type: String, required: false },
+    coverPageType: { type: String, required: false },
+    createdAt: { type: Date, default: Date.now, required: true },
 });
 
+TextbookSchema.virtual('coverImagePath').get(function () {
+    if (this.coverImage != null && this.coverImageType != null) {
+        return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString(
+            'base64'
+        )}`;
+    }
+});
 const Textbook = model<IBookSchema>('Textbook', TextbookSchema);
 
 export default Textbook;
