17753bb7a32b6490951d8daadc8721358b891d6c
batanoffs <batanoff.s@protonmail.com>
2024-07-25 22:00:44 +0300

    updated port

--- server/src/constants/serverSetup.ts
+++ server/src/constants/serverSetup.ts
@@ -1,3 +1,3 @@
-export const PORT = process.env.PORT || 5002;
+export const PORT = process.env.PORT || 5001;
 export const connectionString = process.env.MONGO_URI || 'mongodb://localhost:27017/bookstore';
 export const ALLOWED_ADDRESS = "http://localhost:5173";
