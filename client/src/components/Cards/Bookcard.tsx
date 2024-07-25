21faf5019b4549b6f8f68e6ebfbdfeb155d3ce7d
batanoffs <batanoff.s@protonmail.com>
2024-07-25 21:59:40 +0300

    added null check

--- client/src/components/Cards/Bookcard.tsx
+++ client/src/components/Cards/Bookcard.tsx
@@ -1,44 +1,79 @@
-import { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import Rating from '@mui/material/Rating';
 import Skeleton from '@mui/material/Skeleton';
 
 export const Bookcard = ({ book, loading = false }) => {
-    const [elevation, setElevation] = useState(0);
-    const { id, imageUrl, title, author, rating, price } = book;
+    const { id, imageUrl, title, author, price } = book ?? {};
+
+    if (!book) return null;
 
     return (
         <Card
-            sx={{ maxWidth: 130 }}
-            elevation={elevation}
-            onMouseOver={() => setElevation(10)}
-            onMouseOut={() => setElevation(0)}
+            sx={{ maxWidth: 140, cursor: 'pointer', borderRadius: '0px', boxShadow: 'none' }}
             key={id}
         >
             {loading ? (
                 <Skeleton sx={{ height: 190 }} animation="wave" variant="rectangular" />
             ) : (
-                <CardMedia className="aspect-[2/3]" component="img" alt={title} image={imageUrl} />
+                <CardMedia component="img" alt={title} image={imageUrl} />
             )}
-            <CardContent className="px-1 pb-2">
+
+            <CardContent sx={{ padding: '0.5em' }}>
                 {loading ? (
-                    <>
+                    <div>
                         <Skeleton animation="wave" variant="text" height={10} />
                         <Skeleton animation="wave" variant="text" height={10} width="80%" />
-                    </>
+                    </div>
                 ) : (
                     <>
-                        <Typography gutterBottom variant="h6" component="div">
+                        <Typography
+                            gutterBottom
+                            variant="h6"
+                            component="div"
+                            sx={{
+                                fontSize: 'small',
+                                fontWeight: 800,
+                                textAlign: 'center',
+                                height: '3em',
+                                overflow: 'hidden',
+                                textOverflow: 'ellipsis',
+                                WebkitLineClamp: 2,
+                                WebkitBoxOrient: 'vertical',
+                                MozBoxOrient: 'vertical',
+                                boxOrient: 'vertical',
+                                marginBottom: '1em',
+                                cursor: 'pointer',
+                            }}
+                        >
                             {title}
                         </Typography>
-                        <Typography gutterBottom variant="body2" component="div">
+                        <Typography
+                            gutterBottom
+                            variant="body2"
+                            component="div"
+                            sx={{
+                                fontSize: 'small',
+                                fontWeight: 400,
+                                textAlign: 'center',
+                                height: '1.4em',
+                                overflow: 'hidden',
+                                textOverflow: 'ellipsis',
+                                WebkitLineClamp: 2,
+                                WebkitBoxOrient: 'vertical',
+                                MozBoxOrient: 'vertical',
+                                boxOrient: 'vertical',
+                                marginBottom: '1em',
+                            }}
+                        >
                             {author}
                         </Typography>
-                        <Rating name="read-only" value={rating} precision={0.5} readOnly />
-                        <Typography variant="body2" color="text.secondary">
+                        <Typography
+                            variant="body2"
+                            color="text.secondary"
+                            sx={{ textAlign: 'center' }}
+                        >
                             {price} лв.
                         </Typography>
                     </>
