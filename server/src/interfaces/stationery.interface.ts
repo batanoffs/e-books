4b6757ee55cb8fd29759409a1a0ba3aed8bb172c
batanoffs <batanoff.s@protonmail.com>
2024-07-25 22:01:46 +0300

    updated interface

--- server/src/interfaces/stationery.interface.ts
+++ server/src/interfaces/stationery.interface.ts
@@ -2,7 +2,9 @@ export interface IStationerySchema {
     title: string;
     price: number;
     description: string;
-    images: [string];
+    coverImage: Buffer; //TODO update logic for multiple images
+    coverImageType: String;
     stock: number;
     category: string;
+    createdAt: Date;
 }
