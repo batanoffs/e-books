c6f39b09c6613abac6b7b88f14eab99d16e0a6e0
batanoffs <batanoff.s@protonmail.com>
2024-07-27 11:02:14 +0300

    added bodyparser config to express

--- server/src/config/express.ts
+++ server/src/config/express.ts
@@ -1,5 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+import bodyParser from 'body-parser';
 import { Express } from 'express';
 
 import { secret } from '../constants/identity';
@@ -10,6 +11,8 @@ function configExpress(app: Express): void {
     app.use(cookieParser({ secret }));
     app.use(session());
     app.use(cors());
+    app.use(bodyParser.json({ limit: '10mb' }));
+    app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
     app.use(express.json());
 }
 
