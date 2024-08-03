8df4cd5767228cecb14a55717f35a3188fa7e0c0
batanoffs <batanoff.s@protonmail.com>
2024-08-03 20:33:45 +0300

    added routes

--- client/src/utils/constants/api.ts
+++ client/src/utils/constants/api.ts
@@ -1,13 +1,15 @@
 const baseURL = 'http://localhost:5001';
 
 export const API = {
-    LOGIN: baseURL + '/api/login',
-    LOGOUT: baseURL + '/api/logout',
-    REGISTER: baseURL + '/api/register',
-    BOOKS: baseURL + '/api/books',
-    TEXTBOOKS: baseURL + '/api/textbooks',
-    STATIONERY: baseURL + '/api/stationery',
-    CATEGORIES: baseURL + '/api/categories',
-    ORDERS: baseURL + '/api/orders',
-    USERS: baseURL + '/api/users',
+    LOGIN: baseURL + '/api/login/',
+    LOGOUT: baseURL + '/api/logout/',
+    REGISTER: baseURL + '/api/register/',
+    BOOKS: baseURL + '/api/books/',
+    TEXTBOOKS: baseURL + '/api/textbooks/',
+    STATIONERY: baseURL + '/api/stationery/',
+    CATEGORIES: baseURL + '/api/categories/',
+    ORDERS: baseURL + '/api/orders/',
+    USERS: baseURL + '/api/users/',
+    USER_ID: baseURL + '/api/users/user-id',
+    CART: baseURL + '/api/cart/',
 };
