9f74d5616624a8c50af97a73a60b1be54e0613f8
batanoffs <batanoff.s@protonmail.com>
2024-08-03 20:30:16 +0300

    renamed and updated cart model

---
+++ server/src/models/Cart.ts
@@ -0,0 +1,51 @@
+import { model, Schema } from 'mongoose'
+import { IShoppingCart } from '../interfaces/shoppingcart.interface'
+
+const CartProductSchema: Schema = new Schema(
+	{
+		productType: { 
+            type: String, 
+            required: true 
+        },
+		productId: { 
+            type: Schema.Types.ObjectId, 
+            required: true 
+        },
+		quantity: { 
+            type: Number, 
+            required: true 
+        },
+		name: { 
+            type: String, 
+            required: true 
+        },
+		price: { 
+            type: Number, 
+            required: true 
+        },
+	},
+	{ _id: false }
+)
+
+const ShoppingCartSchema: Schema = new Schema(
+	{
+		userId: { 
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true },
+		products: [CartProductSchema],
+		active: { 
+            type: Boolean, 
+            default: true 
+        },
+		modifiedAt: { 
+            type: Date, 
+            default: Date.now 
+        },
+	},
+	{ timestamps: true }
+)
+
+const Cart = model<IShoppingCart>('Cart', ShoppingCartSchema)
+
+export default Cart
