437a45111f0f43d283fc6c850b6f6cc6e706b519
batanoffs <batanoff.s@protonmail.com>
2024-08-03 20:33:31 +0300

    updated helper get user id and role

--- client/src/utils/helpers/auth.ts
+++ client/src/utils/helpers/auth.ts
@@ -1,39 +1,75 @@
+import axios from 'axios'
+import { API } from '../constants/api'
+
 const isGuest = (): boolean => {
-    const token = getToken();
-    return !token;
-};
+	const token = getToken()
+	return !token
+}
 
 const isAuth = (): boolean => {
-    return !isGuest();
-};
+	return !isGuest()
+}
 
 const getToken = (): string | undefined => {
-    return document.cookie
-        .split(';')
-        .map((cookie) => cookie.trim())
-        .find((cookie) => cookie.startsWith('token='))
-        ?.split('=')[1];
-};
+	return document.cookie
+		.split(';')
+		.map((cookie) => cookie.trim())
+		.find((cookie) => cookie.startsWith('token='))
+		?.split('=')[1]
+}
 
 const getUserRole = async () => {
-    const token = getToken();
-    if (!token) {
-        return null;
-    }
-    //TODO decode token
-    const decodedToken = jwt_decode<{ role: string }>(token);
+	const token = getToken()
+	if (!token) {
+		console.error('No token found in cookies')
+		return
+	}
+	try {
+		const responseFromUser = await axios.get(API.USERS, {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		})
+		const userRole = responseFromUser.data.role
 
-    return decodedToken.role;
-};
+		console.log('User Role:', userRole)
+
+		return userRole
+	} catch (error) {
+		console.error('Error fetching user ID:', error)
+	}
+}
+
+const getUserId = async () => {
+	const token = getToken()
+	if (!token) {
+		console.error('No token found in cookies')
+		return
+	}
+	try {
+		const responseFromUser = await axios.get(API.USER_ID, {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		})
+		const userId = responseFromUser.data.userId
+
+		console.log('User ID:', userId)
+
+		return userId
+	} catch (error) {
+		console.error('Error fetching user ID:', error)
+	}
+}
 
 const checkIfUserIsAdmin = () => {
-    const userRole = getUserRole(); // Get user role
+	const userRole = getUserRole() // Get user role
 
-    // Check if user has admin role
-    if (!userRole) {
-        return false;
-    }
-    return true;
-};
+	// Check if user has admin role
+	if (!userRole) {
+		return false
+	}
+	return true
+}
 
-export { isGuest, isAuth, getToken, checkIfUserIsAdmin };
+export { isGuest, isAuth, getToken, checkIfUserIsAdmin, getUserId, getUserRole }
