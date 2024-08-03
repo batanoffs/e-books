ef9afa745e05d9805dafcb20603f74130f282de1
batanoffs <batanoff.s@protonmail.com>
2024-08-03 20:30:49 +0300

    updated order model

--- server/src/models/Order.ts
+++ server/src/models/Order.ts
@@ -1,24 +1,38 @@
-import { model, Schema } from 'mongoose';
-import { IOrderSchema } from '../interfaces/order.interface';
+import { model, Schema } from 'mongoose'
+import { IOrderSchema } from '../interfaces/order.interface'
 
 const OrderSchema: Schema = new Schema({
-    userId: { type: String, required: true },
-    products: [
-        {
-            productId: { type: Schema.Types.ObjectId, required: true, refPath: 'productType' },
-            productType: { type: String, required: true, enum: ['Book', 'Textbook', 'Stationery'] },
-            quantity: { type: Number, required: true },
-        },
-    ],
-    total: { type: Number, required: true },
-    status: {
-        type: String,
-        required: true,
-        enum: ['pending', 'shipped', 'delivered'],
-        default: 'pending',
-    },
-});
+	userId: {
+		type: String,
+		required: true,
+	},
+	products: [
+		{
+			productId: {
+				type: Schema.Types.ObjectId,
+				required: true,
+				refPath: 'productType',
+			},
+			productType: {
+				type: String,
+				required: true,
+				enum: ['Book', 'Textbook', 'Stationery'],
+			},
+			quantity: { type: Number, required: true },
+		},
+	],
+	total: {
+		type: Number,
+		required: true,
+	},
+	status: {
+		type: String,
+		required: true,
+		enum: ['pending', 'shipped', 'delivered'],
+		default: 'pending',
+	},
+})
 
-const Order = model<IOrderSchema>('Order', OrderSchema);
+const Order = model<IOrderSchema>('Order', OrderSchema)
 
-export default Order;
+export default Order
