2bcdbfe8002a3fd0f9acd9af824d8089358873a4
batanoffs <batanoff.s@protonmail.com>
2024-08-04 10:43:53 +0300

    updated errors

--- client/src/pages/details/ProductDetails.tsx
+++ client/src/pages/details/ProductDetails.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 import axios from 'axios'
 
 import { ProductDetailsProps } from '../../interfaces/ProductDetailsProps.interface'
 import { ItemDetailsDropdownMenus } from './ItemDetailsDropdownMenus'
 import { ItemDetailsTitle } from './ItemDetailsTitle'
+import { getUserId } from '../../utils/helpers/auth'
 import { API } from '../../utils/constants/api'
 
 import styles from './details.module.scss'
-import { getUserId } from '../../utils/helpers/auth'
 
 export const ProductDetails = ({ ...props }: { props: ProductDetailsProps }) => {
 	const [isDescriptionOpen, setIsDescriptionOpen] = useState<boolean>(true)
@@ -20,15 +20,11 @@ export const ProductDetails = ({ ...props }: { props: ProductDetailsProps }) => 
 
 	const handleAddToCart = async () => {
 		const { _id, price, title } = props
-		if (!_id) return
-		if (!price) return
-		if (!title) return
+
+		if (!_id || !price || !title) return new Error('Missing product data')
 
 		try {
 			const userId = await getUserId()
-
-			if (!userId) return new Error('Error getting user ID')
-
 			const data = {
 				userId,
 				productId: _id,
@@ -38,10 +34,7 @@ export const ProductDetails = ({ ...props }: { props: ProductDetailsProps }) => 
 				productType: 'book',
 			}
 
-			console.log('cart data:', data)
-
-			const response = await axios.post(API.CART, data)
-			console.log(response)
+			await axios.post(API.CART, data)
 		} catch (error) {
 			console.error('Error getting user ID or adding to cart:', error)
 		}
@@ -51,45 +44,40 @@ export const ProductDetails = ({ ...props }: { props: ProductDetailsProps }) => 
 		//TODO buy now
 		console.log('Buy now')
 		// navigate('/checkout')
-		// await axios.put(`/cart/${_id}`, { quantity: 1 });
 	}
 
 	const handleAddToWishlist = async () => {
 		//TODO buy now
 		console.log('Added to wishlist')
-
-		// await axios.put(`/cart/${_id}`, { quantity: 1 });
 	}
 
 	return (
 		<div className={styles.detailsWrapper}>
-			<div className={styles.dropdownSection}>
-				<ItemDetailsTitle
-					{...{
-						handleAddToCart,
-						handleBuyNow,
-						handleAddToWishlist,
-						styles,
-						quantity,
-						setQuantity,
-						...props,
-					}}
-				/>
-				<ItemDetailsDropdownMenus
-					{...{
-						styles,
-						isDescriptionOpen,
-						setIsDescriptionOpen,
-						isDeliveryInfoOpen,
-						setIsDeliveryInfoOpen,
-						isReturnsOpen,
-						setIsReturnsOpen,
-						isCommentsOpen,
-						setIsCommentsOpen,
-						...props,
-					}}
-				/>
-			</div>
+			<ItemDetailsTitle
+				{...{
+					handleAddToCart,
+					handleBuyNow,
+					handleAddToWishlist,
+					styles,
+					quantity,
+					setQuantity,
+					...props,
+				}}
+			/>
+			<ItemDetailsDropdownMenus
+				{...{
+					styles,
+					isDescriptionOpen,
+					setIsDescriptionOpen,
+					isDeliveryInfoOpen,
+					setIsDeliveryInfoOpen,
+					isReturnsOpen,
+					setIsReturnsOpen,
+					isCommentsOpen,
+					setIsCommentsOpen,
+					...props,
+				}}
+			/>
 		</div>
 	)
 }
