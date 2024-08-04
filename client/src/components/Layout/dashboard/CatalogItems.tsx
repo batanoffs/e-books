855e5ed67b2999830293bdf428c195d429f278c8
batanoffs <batanoff.s@protonmail.com>
2024-08-04 13:34:32 +0300

    updated code order

--- client/src/components/Layout/dashboard/CatalogItems.tsx
+++ client/src/components/Layout/dashboard/CatalogItems.tsx
@@ -1,20 +1,20 @@
-interface CatalogItems {
-    items: Item[];
-    CardComponent: React.ComponentType<React.PropsWithChildren<{ item: Item }>>;
-    sx?: React.CSSProperties;
+interface Item {
+	_id: string
+	[key: string]: unknown
 }
 
-interface Item {
-    _id: string;
-    [key: string]: unknown;
+interface CatalogItems {
+	items: Item[]
+	CardComponent: React.ComponentType<React.PropsWithChildren<{ item: Item }>>
+	sx?: React.CSSProperties
 }
 
 export const CatalogItems = ({ items, CardComponent, sx }: CatalogItems) => (
-    <div style={sx}>
-        {items.length > 0 ? (
-            items.map((item) => <CardComponent key={item._id} item={item} />)
-        ) : (
-            <p>Няма намерени книги</p>
-        )}
-    </div>
-);
+	<div style={sx}>
+		{items.length > 0 ? (
+			items.map((item) => <CardComponent key={item._id} item={item} />)
+		) : (
+			<p>Няма намерени книги</p>
+		)}
+	</div>
+)
