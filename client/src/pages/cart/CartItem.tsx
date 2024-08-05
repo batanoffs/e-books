d61c7c747061c115736afe140d4f7eb17e6f9d21
batanoffs <batanoff.s@protonmail.com>
2024-08-05 19:09:42 +0300

    updated component handlers

--- client/src/pages/cart/CartItem.tsx
+++ client/src/pages/cart/CartItem.tsx
@@ -1,42 +1,55 @@
-import { Button, Typography, TextField, Grid } from '@mui/material'
+import { Box, Button, Typography, TextField } from '@mui/material'
+import formatCurrencyToBGN from '../../utils/helpers/formatCurrency'
+import { getUserId } from '../../utils/helpers/auth'
 
-const CartItem = ({ item, removeFromCart, updateQuantity }) => (
-	<Grid
-		container
-		spacing={2}
-		alignItems='center'
-		sx={{ borderBottom: '1px solid #ddd', paddingBottom: 2, marginBottom: 2 }}
-	>
-		<Grid item xs={4}>
-			<img src={item.imageUrl} alt={item.name} style={{ width: '100%' }} />
-		</Grid>
-		<Grid item xs={8}>
-			<Typography variant='h6' component='h2'>
-				{item.name}
-			</Typography>
-			<Typography variant='body1' component='p'>
-				Price: {item.price} лв.
-			</Typography>
-			<Typography variant='body1' component='p'>
-				Type: {item.productType}
-			</Typography>
-			<TextField
-				type='number'
-				value={item.quantity}
-				onChange={(e) => updateQuantity(item.productId, parseInt(e.target.value))}
-				inputProps={{ min: 1 }}
-				sx={{ width: '100px', marginY: 1 }}
-			/>
-			<Button
-				variant='contained'
-				color='secondary'
-				onClick={() => removeFromCart(item.productId)}
-				sx={{ marginTop: 1 }}
-			>
-				Remove
-			</Button>
-		</Grid>
-	</Grid>
-)
+const CartItem = ({ item, removeFromCart, updateQuantity }) => {
+	const removeProductFromCart = async () => {
+		const userId = await getUserId()
+		await removeFromCart(item.productId, userId)
+	}
+	return (
+		<Box
+			display='flex'
+			alignItems='center'
+			sx={{ borderBottom: '1px solid #ddd', paddingBottom: 2, marginBottom: 2 }}
+		>
+			<Box sx={{ width: '10%', marginRight: 2 }}>
+				<img src={item.productImagePath} alt={item.name} style={{ width: '80px' }} />
+			</Box>
+			<Box sx={{ width: '30%' }}>
+				<Typography variant='h6' component='h2'>
+					{item.name}
+				</Typography>
+			</Box>
+
+			<Box sx={{ width: '10%' }}>
+				<Typography variant='body1' component='p'>
+					{item.productType}
+				</Typography>
+			</Box>
+
+			<Box sx={{ width: '15%' }}>
+				<TextField
+					type='number'
+					value={item.quantity}
+					onChange={(e) => updateQuantity(item.productId, parseInt(e.target.value))}
+					inputProps={{ min: 1 }}
+					sx={{ width: '100px', marginY: 1 }}
+				/>
+			</Box>
+
+			<Box sx={{ width: '20%' }}>
+				<Typography variant='body1' component='p'>
+					Цена: {formatCurrencyToBGN(item.price * item.quantity)}
+				</Typography>
+			</Box>
+			<Box sx={{ width: '15%' }}>
+				<Button variant='contained' color='error' onClick={removeProductFromCart}>
+					Изтрий
+				</Button>
+			</Box>
+		</Box>
+	)
+}
 
 export default CartItem
--- client/src/pages/cart/CartPage.tsx
+++ client/src/pages/cart/CartPage.tsx
@@ -6,11 +6,13 @@ import { API } from '../../utils/constants/api'
 import useCartStore from '../../store/cart'
 import { getUserId } from '../../utils/helpers/auth'
 import CartItem from './CartItem'
+import formatCurrencyToBGN from '../../utils/helpers/formatCurrency'
 
 const CartPage = () => {
 	const cart = useCartStore((state) => state.cart)
-	const removeFromCart = useCartStore((state) => state.removeFromCart)
 	const updateQuantity = useCartStore((state) => state.updateQuantity)
+	const removeFromCart = useCartStore((state) => state.removeFromCart)
+	const clearCart = useCartStore((state) => state.clearCart)
 
 	useEffect(() => {
 		const fetchCart = async () => {
@@ -28,15 +30,23 @@ const CartPage = () => {
 		fetchCart()
 	}, [])
 
+	const clearCartHandler = async () => {
+		const userId = await getUserId()
+		await clearCart(userId)
+	}
+
 	const total = cart.reduce((sum, item) => sum + item.price * item.quantity, 0)
+	const getTotalItems = () => cart.reduce((sum, item) => sum + item.quantity, 0)
+	const formattedTotal = formatCurrencyToBGN(total)
 
 	return (
 		<Box
 			sx={{
 				display: 'flex',
 				justifyContent: 'center',
-				alignItems: 'center',
-				flexDirection: 'column',
+				gap: 5,
+				alignItems: 'flex-start',
+				flexDirection: 'row',
 				minHeight: '90vh',
 				padding: 2,
 			}}
@@ -44,16 +54,17 @@ const CartPage = () => {
 			<Paper
 				sx={{
 					width: '100%',
+					minWidth: 400,
 					maxWidth: 800,
 					padding: 2,
 				}}
 			>
 				{cart.length === 0 ? (
 					<Typography variant='body1' component='p' align='center'>
-						Your cart is empty
+						Вашата количка е празна
 					</Typography>
 				) : (
-					<Grid container spacing={2}>
+					<Grid container spacing={0}>
 						{cart.map((item) => (
 							<Grid item xs={12} key={item.productId}>
 								<CartItem
@@ -64,23 +75,49 @@ const CartPage = () => {
 							</Grid>
 						))}
 
-						<Grid item xs={12}>
-							<Box
-								sx={{
-									display: 'flex',
-									justifyContent: 'space-between',
-									alignItems: 'center',
-									marginTop: 2,
-								}}
-							>
-								<Typography variant='h6'>Total: {total} лв.</Typography>
-								<Button variant='contained' color='primary' size='large'>
-									Checkout
-								</Button>
-							</Box>
-						</Grid>
+						<Box
+							sx={{
+								display: 'flex',
+								justifyContent: 'space-between',
+								gap: 4,
+								alignItems: 'center',
+								marginX: 'auto',
+							}}
+						>
+							<Typography variant='h6'>
+								Общо избрани артикула: {getTotalItems()}
+							</Typography>
+							<Button variant='contained' color='error' onClick={clearCartHandler}>
+								Изчистване на количката
+							</Button>
+						</Box>
 					</Grid>
 				)}
+			</Paper>
+			<Paper
+				sx={{
+					width: '100%',
+					minWidth: 200,
+					maxWidth: 300,
+					padding: 2,
+				}}
+			>
+				<Grid item xs={12}>
+					<Box
+						sx={{
+							display: 'flex',
+							flexDirection: 'column',
+							gap: 4,
+							justifyContent: 'space-between',
+							alignItems: 'center',
+						}}
+					>
+						<Typography variant='h6'>Общо: {formattedTotal}</Typography>
+						<Button variant='contained' color='info' size='large'>
+							Плащане
+						</Button>
+					</Box>
+				</Grid>
 			</Paper>
 		</Box>
 	)
