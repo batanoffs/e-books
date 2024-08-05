abf491022b9f2f2d7708db9556c14a68143a1d79
batanoffs <batanoff.s@protonmail.com>
2024-08-05 19:00:25 +0300

    changed comment

--- server/src/types/express.d.ts
+++ server/src/types/express.d.ts
@@ -4,11 +4,10 @@ import { File } from 'multer';
 declare module 'express-serve-static-core' {
   interface Request {
     body: MyRequestBodyType;
-    files: File[] | null; // Multiple file upload
+    files: File[] | null; // TODO check if files need to be in the body
   }
 }
 
 interface MyRequestBodyType {
   // Define the types for your form fields here
-  exampleField: string;
 }
