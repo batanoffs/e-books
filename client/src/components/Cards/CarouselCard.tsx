019c51056342476d77afdcdda48ab2e4c41044d4
batanoffs <batanoff.s@protonmail.com>
2024-08-05 19:11:47 +0300

    updated to use currenctFormatter for price

--- client/src/components/Cards/CarouselCard.tsx
+++ client/src/components/Cards/CarouselCard.tsx
@@ -1,40 +1,42 @@
-import { Button } from '@mui/material';
+import { Button } from '@mui/material'
+import formatCurrencyToBGN from '../../utils/helpers/formatCurrency'
 
 interface CarouselCard {
-    book: {
-        _id: string;
-        title: string;
-        author: string;
-        coverImagePath: string;
-        description: string;
-        price: number;
-        category: string;
-        stock: number;
-    };
-    styles: typeof styles;
+	book: {
+		_id: string
+		title: string
+		author: string
+		coverImagePath: string
+		description: string
+		price: number
+		category: string
+		stock: number
+	}
+	styles: Record<string, string>
 }
 
 export const CarouselCard = ({ book, styles }: CarouselCard) => {
-    const { coverImagePath, title, author, price, description, _id } = book;
-    return (
-        <div className={styles.container}>
-            <div className={styles.textContainer}>
-                <h1>{title}</h1>
-                <p>{author}</p>
-                <p>{price} лв.</p>
-                <span>{description}</span>
-                <div className={styles.buttonsContainer}>
-                    <Button variant="contained" className={styles.button}>
-                        Купи
-                    </Button>
-                    <Button variant="contained" className={styles.button} href={`/books/${_id}`}>
-                        Виж детайли
-                    </Button>
-                </div>
-            </div>
-            <div>
-                <img className={styles.image} src={coverImagePath} alt={title} />
-            </div>
-        </div>
-    );
-};
+	const { coverImagePath, title, author, price, description, _id } = book
+	const formatCurrency = formatCurrencyToBGN(price)
+	return (
+		<div className={styles.container}>
+			<div className={styles.textContainer}>
+				<h1>{title}</h1>
+				<p>{author}</p>
+				<p>{formatCurrency}</p>
+				<span>{description}</span>
+				<div className={styles.buttonsContainer}>
+					<Button variant='contained' className={styles.button}>
+						Купи
+					</Button>
+					<Button variant='contained' className={styles.button} href={`/books/${_id}`}>
+						Виж детайли
+					</Button>
+				</div>
+			</div>
+			<div>
+				<img className={styles.image} src={coverImagePath} alt={title} />
+			</div>
+		</div>
+	)
+}
