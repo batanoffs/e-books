8aaf4c301dc68d5b17012a7a3f6269aa36e3954e
batanoffs <batanoff.s@protonmail.com>
2024-08-05 19:02:06 +0300

    extracted expireseIn value to constant for better control in the future

--- server/src/constants/serverSetup.ts
+++ server/src/constants/serverSetup.ts
@@ -1,3 +1,4 @@
 export const PORT = process.env.PORT || 5001;
 export const connectionString = process.env.MONGO_URI || 'mongodb://localhost:27017/bookstore';
 export const ALLOWED_ADDRESS = "http://localhost:5173";
+export const tokenExpiration = '12h';
--- server/src/services/jwt.ts
+++ server/src/services/jwt.ts
@@ -1,23 +1,24 @@
-import { JwtPayload, SignOptions, verify, sign } from 'jsonwebtoken';
-import { secret } from '../constants/identity';
+import { JwtPayload, SignOptions, verify, sign } from 'jsonwebtoken'
+import { tokenExpiration } from '../constants/serverSetup'
+import { secret } from '../constants/identity'
 
 function createToken(userData: { email: string; _id?: string }): string {
-    const payload: JwtPayload = {
-        email: userData.email,
-        _id: userData._id,
-    };
+	const payload: JwtPayload = {
+		email: userData.email,
+		_id: userData._id,
+	}
 
-    const token = sign(payload, process.env.JWT_SECRET || secret, {
-        expiresIn: '1h',
-    } as SignOptions);
+	const token = sign(payload, process.env.JWT_SECRET || secret, {
+		expiresIn: tokenExpiration,
+	} as SignOptions)
 
-    return token;
+	return token
 }
 
 function verifyToken(token: string): JwtPayload {
-    const data = verify(token, secret) as JwtPayload;
+	const data = verify(token, secret) as JwtPayload
 
-    return data;
+	return data
 }
 
-export { createToken, verifyToken };
+export { createToken, verifyToken }
