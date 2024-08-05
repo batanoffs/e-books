b4ad3cecf0a2f50e7307e691bd55faa9e23f3863
batanoffs <batanoff.s@protonmail.com>
2024-08-05 19:04:58 +0300

    added productType and productImage. Also added virtual to save images updated import name

--- server/src/models/Cart.ts
+++ server/src/models/Cart.ts
@@ -1,51 +1,68 @@
 import { model, Schema } from 'mongoose'
-import { IShoppingCart } from '../interfaces/shoppingcart.interface'
+import { IShoppingCart } from '../interfaces/cart.interface'
 
 const CartProductSchema: Schema = new Schema(
 	{
-		productType: { 
-            type: String, 
-            required: true 
-        },
-        productImage: {
-            type: String,
-            required: true
-        },
-		productId: { 
-            type: Schema.Types.ObjectId, 
-            required: true 
-        },
-		quantity: { 
-            type: Number, 
-            required: true 
-        },
-		name: { 
-            type: String, 
-            required: true 
-        },
-		price: { 
-            type: Number, 
-            required: true 
-        },
+		productType: {
+			type: String,
+			required: true,
+		},
+		productImage: {
+			type: Buffer,
+			required: true,
+		},
+		productImageType: {
+			type: String,
+			required: true,
+		},
+		productId: {
+			type: Schema.Types.ObjectId,
+			required: true,
+		},
+		quantity: {
+			type: Number,
+			required: true,
+		},
+		name: {
+			type: String,
+			required: true,
+		},
+		price: {
+			type: Number,
+			required: true,
+		},
 	},
 	{ _id: false }
 )
 
+CartProductSchema.virtual('productImagePath').get(function () {
+    if (this.productImage != null && this.productImageType != null) {
+        return `data:${this.productImageType};charset=utf-8;base64,${this.productImage.toString(
+            'base64'
+        )}`;
+    }
+});
+
+CartProductSchema.set('toJSON', {
+    virtuals: true,
+});
+
 const ShoppingCartSchema: Schema = new Schema(
 	{
-		userId: { 
-            type: Schema.Types.ObjectId,
-            ref: 'User',
-            required: true },
+		userId: {
+			type: Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
 		products: [CartProductSchema],
-		active: { 
-            type: Boolean, 
-            default: true 
-        },
-		modifiedAt: { 
-            type: Date, 
-            default: Date.now 
-        },
+		active: {
+			type: Boolean,
+			default: true,
+		},
+		modifiedAt: {
+			type: Date,
+			default: Date.now,
+		},
 	},
 	{ timestamps: true }
 )
