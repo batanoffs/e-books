992734158b456a195508a8bb145386907535fd47
batanoffs <batanoff.s@protonmail.com>
2024-08-05 19:08:08 +0300

    updated validation

--- client/src/services/authService.ts
+++ client/src/services/authService.ts
@@ -4,13 +4,16 @@ import { getToken } from '../utils/helpers/auth'
 
 const login = async (email: string, password: string) => {
 	if (!email || !password) {
-		throw new Error('Имейл и парола са задъжителни')
+		throw new Error('Email and password are required')
 	}
+
 	try {
 		const response = await axios.post(API.LOGIN, { email, password })
 		const { token, redirectUrl, message } = response.data
-		if (!token || !redirectUrl || !message)
+
+		if (!token || !redirectUrl || !message) {
 			throw new Error('Response token, redirectUrl or message is empty')
+		}
 
 		const currentToken = getToken()
 
@@ -20,9 +23,11 @@ const login = async (email: string, password: string) => {
 		} else {
 			console.warn('Token is already up to date. Skipping...')
 		}
+
 		return { redirectUrl, message }
 	} catch (error) {
 		console.error('Login failed', error)
+		throw error
 	}
 }
 
@@ -35,25 +40,32 @@ const register = async (credentials: {
 	if (!credentials.email || !credentials.password) {
 		throw new Error('Email and password are required')
 	}
+
 	try {
 		const response = await axios.post(API.REGISTER, credentials)
-		console.log('response-data', response.data)
+
+		if (!response.data) throw new Error('Response data is empty')
+
 		return response.data
 	} catch (error) {
 		console.error('Registration failed', error)
+		throw error
 	}
 }
 
 const logout = async () => {
 	try {
 		const response = await axios.get(API.LOGOUT)
+
+		if (!response.data) throw new Error('Response data is empty')
+
 		const { redirectUrl, message } = response.data
-		// Clear token from cookies or local storage
 		document.cookie = 'token=;'
+		
 		return { redirectUrl, message }
 	} catch (error) {
-		// Handle logout error
-		throw new Error('Logout failed')
+		console.error('Logout failed', error)
+		throw error
 	}
 }
 
