b5b89e12622b1dee123ae89a1b447951ca58a35f
batanoffs <batanoff.s@protonmail.com>
2024-08-06 19:41:02 +0300

    updated cart layout

---
+++ client/src/components/Layout/cart/CartLayout.tsx
@@ -0,0 +1,20 @@
+import styles from './cart.module.scss'
+
+type CartLayoutProps = {
+	children: JSX.Element
+	header: JSX.Element
+	aside: JSX.Element
+	[key: string]: any
+}
+
+export const CartLayout = ({ children, header, aside, ...props }: CartLayoutProps) => {
+	return (
+		<main className='main-wrapper'>
+			{header && <header>{header}</header>}
+			<section className={styles.cartContainer}>
+				<article>{children}</article>
+				<aside>{aside}</aside>
+			</section>
+		</main>
+	)
+}
