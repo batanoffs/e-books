e1b718d6c66f647892a71ae202c3bcc6ba4f9073
batanoffs <batanoff.s@protonmail.com>
2024-08-09 20:12:01 +0300

    updated component

--- client/src/pages/checkout/AddressForm.tsx
+++ client/src/pages/checkout/AddressForm.tsx
@@ -1,33 +1,29 @@
-import { Box,Button,FormControl,Grid,InputLabel,MenuItem,Select,TextField } from '@mui/material'
+import {
+	Box,
+	Typography,
+	FormControl,
+	Grid,
+	InputLabel,
+	MenuItem,
+	Select,
+	TextField,
+} from '@mui/material'
 import { Controller } from 'react-hook-form'
-import { locationOptions } from '../../utils/constants/location'
+import { regionOptions } from '../../utils/constants/paymentFormConstants'
 import { InputFormField } from '../../components/InputFormField/InputFormField'
 
-export const AddressForm = ({ control }) => {
-
+export const AddressForm = ({ control, errors }) => {
 	return (
-		<Box component='div'>
-			<h5> Адрес за доставка </h5>
-
+		<Box component='li' id='address-form'>
+			<Typography variant='h5'>Адрес за доставка</Typography>
 			<Grid container spacing={2}>
-				<Grid item xs={12}>
-					<InputFormField
-						label='Имейл адрес'
-						name='username'
-						type='email'
-						required
-						tooltip='Ще изпратим потвърждение на поръчката тук.'
-						control={control}
-					/>
-				</Grid>
-
 				<Grid item xs={12} sm={6}>
 					<InputFormField
 						label='Име'
 						name='firstname'
 						required
-						tooltip='Въпроси за доставка.'
 						control={control}
+						errors={errors?.firstname}
 					/>
 				</Grid>
 				<Grid item xs={12} sm={6}>
@@ -35,50 +31,27 @@ export const AddressForm = ({ control }) => {
 						label='Фамилия'
 						name='lastname'
 						required
-						tooltip='Въпроси за доставка.'
 						control={control}
+						errors={errors?.lastname}
 					/>
 				</Grid>
-
 				<Grid item xs={12} sm={6}>
 					<InputFormField
 						label='Телефон'
 						name='telephone'
 						required
-						tooltip='Въпроси за доставка.'
 						control={control}
+						errors={errors?.telephone}
 					/>
 				</Grid>
 				<Grid item xs={12} sm={6}>
 					<InputFormField
-						label='Post code'
+						label='Пощенски код'
 						name='postcode'
-						tooltip='Въпроси за доставка.'
 						control={control}
+						errors={errors?.postcode}
 					/>
 				</Grid>
-
-				<Grid item xs={12} sm={6}>
-					<FormControl fullWidth margin='normal'>
-						<InputLabel>Държава</InputLabel>
-						<Controller
-							name='country_id'
-							control={control}
-							defaultValue='BG'
-							render={({ field }) => (
-								<Select {...field}>
-									<MenuItem value='BG'>България</MenuItem>
-									{locationOptions.countryOptions.map((option, index) => (
-										<MenuItem key={index} value={option.value}>
-											{option.label}
-										</MenuItem>
-									))}
-								</Select>
-							)}
-						/>
-					</FormControl>
-				</Grid>
-
 				<Grid item xs={12} sm={6}>
 					<FormControl fullWidth margin='normal'>
 						<InputLabel>Област / Провинция</InputLabel>
@@ -87,8 +60,8 @@ export const AddressForm = ({ control }) => {
 							control={control}
 							render={({ field }) => (
 								<Select {...field}>
-									{locationOptions.regionOptions.map((option, index) => (
-										<MenuItem key={index} value={option.value}>
+									{regionOptions.map((option) => (
+										<MenuItem key={option.value} value={option.value}>
 											{option.label}
 										</MenuItem>
 									))}
@@ -97,56 +70,31 @@ export const AddressForm = ({ control }) => {
 						/>
 					</FormControl>
 				</Grid>
-
 				<Grid item xs={12} sm={6}>
 					<InputFormField
 						label='Населено място'
 						name='city'
-						tooltip='град'
 						control={control}
+						errors={errors?.city}
 					/>
 				</Grid>
-				<Grid item xs={12} sm={6}>
+				<Grid item xs={12}>
 					<InputFormField
 						label='Адрес'
-						name='street[0]'
-						tooltip='адрес'
+						name='street'
 						control={control}
+						errors={errors?.street}
 					/>
 				</Grid>
-
-				<Grid item xs={12} sm={6}>
-					<InputFormField
-						label='boxnow_id'
-						name='custom_attributes[boxnow_id]'
-						tooltip=''
-						control={control}
-					/>
-				</Grid>
-				<Grid item xs={12} sm={6}>
+				<Grid item xs={12}>
 					<FormControl fullWidth margin='normal'>
-						<InputLabel>Shipping delivery data</InputLabel>
+						<InputLabel>Допълнителна информация за доставка</InputLabel>
 						<Controller
-							name='custom_attributes[shiping_delivery_data]'
+							name='custom_attributes.shiping_delivery_data'
 							control={control}
 							render={({ field }) => <TextField {...field} multiline rows={2} />}
 						/>
 					</FormControl>
-				</Grid>
-
-				<Grid item xs={12}>
-					<InputFormField
-						label='Shipping delivery method data'
-						name='custom_attributes[shipping_delivery_method_data]'
-						tooltip='метод за доставка'
-						control={control}
-					/>
-				</Grid>
-
-				<Grid item xs={12}>
-					<Button type='submit' variant='contained' fullWidth>
-						Submit
-					</Button>
 				</Grid>
 			</Grid>
 		</Box>
