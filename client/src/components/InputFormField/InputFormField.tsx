a69dd64c7741933c6822972b41cb82122524a7b5
batanoffs <batanoff.s@protonmail.com>
2024-08-09 20:11:07 +0300

    updated component

--- client/src/components/InputFormField/InputFormField.tsx
+++ client/src/components/InputFormField/InputFormField.tsx
@@ -1,27 +1,39 @@
 import { FormControl, TextField } from '@mui/material'
 import { Controller } from 'react-hook-form'
 
+type InputFormFieldProps = {
+	label: string
+	name: string
+	type?: string
+	required?: boolean
+	control: any
+	errors?: any
+}
+
 export const InputFormField = ({
 	label,
 	name,
 	type = 'text',
 	required = false,
-	tooltip,
 	control,
-}) => (
-	<FormControl fullWidth margin='normal'>
-		<Controller
-			name={name}
-			control={control}
-			render={({ field }) => (
-				<TextField
-					{...field}
-					label={label}
-					type={type}
-					required={required}
-					helperText={tooltip}
-				/>
-			)}
-		/>
-	</FormControl>
-)
+	errors,
+}: InputFormFieldProps) => {
+	return (
+		<FormControl fullWidth margin='normal'>
+			<Controller
+				name={name}
+				control={control}
+				render={({ field }) => (
+					<TextField
+						{...field}
+						label={label}
+						type={type}
+						required={required}
+						error={Boolean(errors)}
+						helperText={errors?.message}
+					/>
+				)}
+			/>
+		</FormControl>
+	)
+}
