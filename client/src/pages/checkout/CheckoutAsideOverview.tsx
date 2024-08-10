e29f48ff80dd194638fbc8c373eb2128c80a7330
batanoffs <batanoff.s@protonmail.com>
2024-08-10 22:03:05 +0300

    updated cart page order overview and buttons

--- client/src/pages/checkout/CheckoutAsideOverview.tsx
+++ client/src/pages/checkout/CheckoutAsideOverview.tsx
@@ -1,18 +1,86 @@
-export const CheckoutOverview = ({ cart, handlePlaceOrder }) => {
-	return (
-		<>
-			<h6>Обобщение на поръчката</h6>
-			<ul>
-				{cart.map((product) => (
-					<li key={product.id}>
-						<h2>{product.title}</h2>
-						<p>Price: {product.price}</p>
-						<p>Quantity: {product.quantity}</p>
-					</li>
-				))}
-			</ul>
+import {
+	Box,
+	Button,
+	Paper,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+	Typography,
+} from '@mui/material'
+import formatCurrencyToBGN from '../../utils/helpers/formatCurrency'
+import { useEffect, useState } from 'react'
 
-			<button onClick={handlePlaceOrder}>Поръчай</button>
-		</>
+export const CheckoutOverview = ({ cart, handlePlaceOrder, handleBackToCart }) => {
+	const [totalSum, setTotalSum] = useState(0)
+
+	useEffect(() => {
+		const calculateTotalSum = () => {
+			const total = cart.reduce((acc, product) => {
+				return acc + product.price * product.quantity
+			}, 0)
+			setTotalSum(total)
+		}
+
+		calculateTotalSum()
+	}, [cart])
+	console.log(cart)
+
+	return (
+		<Paper sx={{ padding: '1rem' }}>
+			<Typography variant='h6' align='center'>
+				Обобщение на поръчката
+			</Typography>
+			<TableContainer sx={{ marginTop: '1rem' }}>
+				<Table>
+					<TableHead>
+						<TableRow>
+							<TableCell>Продукт</TableCell>
+							<TableCell>Име</TableCell>
+							<TableCell>Цена</TableCell>
+							<TableCell>Брой</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{cart.map((product) => (
+							<TableRow key={product.id}>
+								<TableCell>
+									<img style={{ width: '50px' }} src={product.productImagePath} />
+								</TableCell>
+								<TableCell
+									sx={{
+										maxWidth: '200px',
+										minWidth: '150px',
+										overflowWrap: 'break-word',
+										wordWrap: 'break-word',
+										wordBreak: 'break-word',
+										lineHeight: '1.2',
+									}}
+								>
+									{product.name}
+								</TableCell>
+								<TableCell>{formatCurrencyToBGN(product.price)}</TableCell>
+								<TableCell>{product.quantity}</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</TableContainer>
+
+			<Typography align='center' variant='h6' sx={{ marginTop: '1rem' }}>
+				Обща стойност: {formatCurrencyToBGN(totalSum)}
+			</Typography>
+
+			<Box sx={{ display: 'flex', justifyContent: 'space-between', marginTop: '1rem' }}>
+				<Button onClick={handleBackToCart} variant='contained' color='primary'>
+					Количка
+				</Button>
+				<Button onClick={handlePlaceOrder} variant='contained' color='secondary'>
+					Завършване на поръчката
+				</Button>
+			</Box>
+		</Paper>
 	)
 }
--- client/src/pages/checkout/CheckoutPage.tsx
+++ client/src/pages/checkout/CheckoutPage.tsx
@@ -1,15 +1,14 @@
 import { useForm, Controller } from 'react-hook-form'
-import { Box, Button } from '@mui/material'
-
+import { useNavigate } from 'react-router-dom'
 import { CommentForOrder } from './CommentForOrder'
 import { CheckoutLayout } from '../../components/Layout/checkout/CheckoutLayout'
 import { CheckoutOverview } from './CheckoutAsideOverview'
 import { DiscountCode } from './DiscountCode'
 
-import useCartStore from '../../store/cart'
 import { DeliveryForm } from './DeliveryForm'
 import { AddressForm } from './AddressForm'
 import { PaymentForm } from './PaymentForm'
+import useCartStore from '../../store/cart'
 
 type CheckoutFormValues = {
 	firstname: string
@@ -29,7 +28,6 @@ type CheckoutFormValues = {
 const CheckoutPage = () => {
 	const cart = useCartStore((state) => state.cart)
 	const placeOrder = useCartStore((state) => state.placeOrder)
-
 	const { control, handleSubmit, formState } = useForm<CheckoutFormValues>({
 		defaultValues: {
 			shippingMethod: '',
@@ -37,40 +35,41 @@ const CheckoutPage = () => {
 		},
 	})
 
+	const navigate = useNavigate()
+
 	const { errors } = formState // Extract the errors from formState
 
 	const handlePlaceOrder = (data: CheckoutFormValues) => {
 		console.table(data)
 
-		placeOrder({
-			id: new Date().toISOString(),
-			items: cart,
-			paymentMethod: data.paymentMethod,
-			deliveryMethod: data.shippingMethod,
-		})
+		// placeOrder({
+		// 	id: new Date().toISOString(),
+		// 	items: cart,
+		// 	paymentMethod: data.paymentMethod,
+		// 	deliveryMethod: data.shippingMethod,
+		// })
+	}
+
+	const handleBackToCart = () => {
+		navigate('/cart')
 	}
 
 	return (
 		<CheckoutLayout
 			aside={
-				<CheckoutOverview cart={cart} handlePlaceOrder={handleSubmit(handlePlaceOrder)} />
+				<CheckoutOverview
+					cart={cart}
+					handleBackToCart={handleBackToCart}
+					handlePlaceOrder={handleSubmit(handlePlaceOrder)}
+				/>
 			}
 		>
 			<form onSubmit={handleSubmit(handlePlaceOrder)}>
 				<AddressForm errors={errors} control={control} />
-
 				<DeliveryForm errors={errors} control={control} />
-
 				<PaymentForm control={control} errors={errors} />
-
 				<DiscountCode />
 				<CommentForOrder control={control} Controller={Controller} />
-
-				<Box mt={3}>
-					<Button type='submit' variant='contained' color='primary'>
-						Place Order
-					</Button>
-				</Box>
 			</form>
 		</CheckoutLayout>
 	)
