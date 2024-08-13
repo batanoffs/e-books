85d2f8f65b98561fcafea6d9ecef8576c99d1f38
batanoffs <batanoff.s@protonmail.com>
2024-08-13 20:42:21 +0300

    clean up

--- client/src/utils/helpers/theme.ts
+++ client/src/utils/helpers/theme.ts
@@ -169,3 +169,35 @@ export const theme = {
 }
 
 export default themeOptions
+
+// import { defaultTheme } from 'react-admin';
+
+// Function to create light and dark mode themes
+// export const myTheme = {
+//     typography: {
+//         fontFamily: ['Montserrat Alternates', 'Arial', 'sans-serif'].join(','),
+//     },
+//     components: {
+//         ...defaultTheme.components,
+//         MuiTextField: {
+//             defaultProps: {
+//                 variant: 'outlined' as const,
+//             },
+//         },
+//         MuiFormControl: {
+//             defaultProps: {
+//                 variant: 'outlined' as const,
+//             },
+//         },
+//         MuiList: {
+//             defaultProps: {
+//                 dense: true,
+//             },
+//         },
+//         MuiTable: {
+//             defaultProps: {
+//                 size: 'small',
+//             },
+//         },
+//     },
+// };
