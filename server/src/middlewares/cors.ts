98625631c15ff72b358f084af983b33d62c75e81
batanoffs <batanoff.s@protonmail.com>
2024-08-13 20:36:03 +0300

    added environment variable for allowed origin

--- server/src/middlewares/cors.ts
+++ server/src/middlewares/cors.ts
@@ -1,22 +1,21 @@
-import { Request, Response, NextFunction } from 'express';
-import { ALLOWED_ADDRESS } from '../constants/serverSetup';
+import { Request, Response, NextFunction } from 'express'
+import { XTotalCount } from '../constants/serverSetup'
 
-const count = '1/1';
 function cors() {
-    return function (req: Request, res: Response, next: NextFunction) {
-        res.setHeader('Access-Control-Allow-Origin', ALLOWED_ADDRESS);
-        res.setHeader('Access-Control-Expose-Headers', 'X-Total-Count');
-        res.setHeader('Access-Control-Allow-Credentials', 'true');
-        res.setHeader('X-Total-Count', count);
+	return function (req: Request, res: Response, next: NextFunction) {
+		res.setHeader('Access-Control-Allow-Origin', process.env.ALLOWED_ADDRESS!)
+		res.setHeader('Access-Control-Expose-Headers', 'X-Total-Count')
+		res.setHeader('Access-Control-Allow-Credentials', 'true')
+		res.setHeader('X-Total-Count', XTotalCount)
 
-        if (req.method === 'OPTIONS') {
-            res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-            res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-            return res.sendStatus(204);
-        }
+		if (req.method === 'OPTIONS') {
+			res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+			res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+			return res.sendStatus(204)
+		}
 
-        next();
-    };
+		next()
+	}
 }
 
-export { cors };
+export { cors }
