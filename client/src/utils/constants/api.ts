f14c1b334fcbaae1fa32fc699b72306a8db3a3ee
batanoffs <batanoff.s@protonmail.com>
2024-08-13 20:43:12 +0300

    added endpoints

--- client/src/utils/constants/api.ts
+++ client/src/utils/constants/api.ts
@@ -1,15 +1,17 @@
-const baseURL = 'http://localhost:5001';
+const baseURL = 'http://localhost:5001'
 
 export const API = {
-    LOGIN: baseURL + '/api/login/',
-    LOGOUT: baseURL + '/api/logout/',
-    REGISTER: baseURL + '/api/register/',
-    BOOKS: baseURL + '/api/books/',
-    TEXTBOOKS: baseURL + '/api/textbooks/',
-    STATIONERY: baseURL + '/api/stationery/',
-    CATEGORIES: baseURL + '/api/categories/',
-    ORDERS: baseURL + '/api/orders/',
-    USERS: baseURL + '/api/users/',
-    USER_ID: baseURL + '/api/users/user-id',
-    CART: baseURL + '/api/cart/',
-};
+	LOGIN: baseURL + '/api/login/',
+	LOGOUT: baseURL + '/api/logout/',
+	REGISTER: baseURL + '/api/register/',
+	BOOKS: baseURL + '/api/books/',
+	TEXTBOOKS: baseURL + '/api/textbooks/',
+	STATIONERY: baseURL + '/api/stationery/',
+	CATEGORIES: baseURL + '/api/categories/',
+	ORDERS: baseURL + '/api/orders/',
+	USERS: baseURL + '/api/users/',
+	USER_ID: baseURL + '/api/users/user-id',
+	CART: baseURL + '/api/cart/',
+	ADMIN: baseURL + '/api/admin/',
+	CHECKOUT: baseURL + '/api/checkout/',
+}
