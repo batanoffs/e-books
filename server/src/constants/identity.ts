2a4f8f98891a9cbbd628f28dd98e0f40648ca482
batanoffs <batanoff.s@protonmail.com>
2024-08-13 20:45:05 +0300

    updated vaiables

--- server/src/constants/identity.ts
+++
@@ -1,6 +0,0 @@
-//TODO change identity based on requirements
-const identityName: string = 'email'
-const secret: string = 'super secret'
-const stripeTestAPI =
-	'sk_test_51PmvUNRsXEpCIyS49IYGvAqFaSjf0xSoptlSoVGTPZknpu9G1g04uKCtDowWCkvs5zVRRMGPzZaGSaZVpSUy7Qbe00p00KkmxN'
-export { identityName, secret, stripeTestAPI }
--- server/src/constants/serverSetup.ts
+++ server/src/constants/serverSetup.ts
@@ -1,4 +1,2 @@
-export const PORT = process.env.PORT || 5001;
-export const connectionString = process.env.MONGO_URI || 'mongodb://localhost:27017/bookstore';
-export const ALLOWED_ADDRESS = "http://localhost:5173";
-export const tokenExpiration = '12h';
+export const ALLOWED_ADDRESS = '*' //"http://localhost:5173";
+export const XTotalCount = '1/1' //TODO update for propagation
