c807016a22c2b61d9b3f0c20ec900a266adbd660
batanoffs <batanoff.s@protonmail.com>
2024-08-14 17:35:22 +0300

    added warning for MU imports

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,23 +1,34 @@
 module.exports = {
-    root: true,
-    env: { browser: true, es2020: true, node: true },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react-hooks/recommended',
-        'plugin:react/recommended',
-        'plugin:react/jsx-runtime',
-        'prettier',
-    ],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['react-refresh', '@typescript-eslint'],
-    rules: {
-        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    },
-    settings: {
-        react: {
-            version: 'detect',
-        },
-    },
-};
+	root: true,
+	env: { browser: true, es2020: true, node: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'prettier',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react-refresh', '@typescript-eslint'],
+	rules: {
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true },
+		],
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	rules: {
+		'no-restricted-imports': [
+			'error',
+			{
+				patterns: ['@mui/*/*/*'],
+			},
+		],
+	},
+}
