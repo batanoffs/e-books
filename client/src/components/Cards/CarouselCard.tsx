360cba3cd7ee14cf158bb8ad147e2d8db2a7c0a6
batanoffs <batanoff.s@protonmail.com>
2024-08-17 17:48:17 +0300

    updated carouselcard handlers

--- client/src/components/Cards/CarouselCard.tsx
+++ client/src/components/Cards/CarouselCard.tsx
@@ -1,6 +1,11 @@
 import Button from '@mui/material/Button'
-
+import { useNavigate } from 'react-router-dom'
 import formatCurrencyToBGN from '../../utils/helpers/formatCurrency'
+import { useAlertStore } from '../../store/alert'
+import { useLoginModal } from '../../store/helperModal'
+import { isAuth } from '../../middlewares/guards'
+import useCartStore from '../../store/cart'
+import { cartService } from '../../services/cartService'
 
 interface CarouselCard {
 	book: {
@@ -19,6 +24,36 @@ interface CarouselCard {
 export const CarouselCard = ({ book, styles }: CarouselCard) => {
 	const { coverImagePath, title, author, price, description, _id } = book
 	const formatCurrency = formatCurrencyToBGN(price)
+	const showAlert = useAlertStore((state) => state.showAlert)
+	const navigate = useNavigate()
+	const toggleOpen = useLoginModal((state) => state.toggleOpen)
+	const addToCart = useCartStore((state) => state.addToCart)
+	const onGoToDetails = () => {
+		navigate(`/catalog/books/all/${_id}`)
+	}
+
+	const onBuyNow = async () => {
+		const isUserAuthenticated = isAuth()
+		if (!isUserAuthenticated) {
+			toggleOpen()
+			return showAlert('Моля, влезте в акаунта си, за да продължите', 'info')
+		}
+
+		const currentItem = {
+			product: {
+				id: _id,
+				coverImagePath: coverImagePath,
+				title: title,
+				price: price,
+			},
+			quantity: 1,
+		}
+		addToCart(currentItem)
+		await cartService.addToCart(book, 1, 'book')
+
+		navigate(`/cart`)
+		showAlert(`Продуктът ${title} е добавен в количката`, 'success')
+	}
 	return (
 		<div className={styles.container}>
 			<div className={styles.textContainer}>
@@ -27,10 +62,15 @@ export const CarouselCard = ({ book, styles }: CarouselCard) => {
 				<p>{formatCurrency}</p>
 				<span>{description}</span>
 				<div className={styles.buttonsContainer}>
-					<Button variant='contained' color='secondary' className={styles.button}>
+					<Button
+						variant='contained'
+						color='secondary'
+						className={styles.button}
+						onClick={onBuyNow}
+					>
 						Купи
 					</Button>
-					<Button variant='contained' className={styles.button} href={`/books/${_id}`}>
+					<Button variant='contained' className={styles.button} onClick={onGoToDetails}>
 						Виж детайли
 					</Button>
 				</div>
