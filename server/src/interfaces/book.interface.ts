1fa038e35efdf1713256ebdc9e775664a45264d4
batanoffs <batanoff.s@protonmail.com>
2024-08-20 19:43:30 +0300

    create book interface

--- server/src/interfaces/book.interface.ts
+++ server/src/interfaces/book.interface.ts
@@ -1,20 +1,20 @@
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose'
 
 export interface IBookSchema extends Document {
-    title: string;
-    author: string;
-    price: number;
-    description: string;
-    coverImage: Buffer; //TODO update logic for multiple images
-    coverImageType: String;
-    stock: number;
-    category: string;
-    publisher?: string;
-    language?: string;
-    publishDate?: Date;
-    pageCount?: number;
-    translator?: string;
-    dimensions?: string;
-    coverPageType?: string;
-    createdAt: Date;
+	title: string
+	author: string
+	price: number
+	description: string
+	coverImage: Buffer //TODO update logic for multiple images
+	coverImageType: String
+	stock: number
+	categories: Types.ObjectId[] // References only book category IDs
+	publisher?: string
+	language?: string
+	publishDate?: Date
+	pageCount?: number
+	translator?: string
+	dimensions?: string
+	coverPageType?: string
+	createdAt: Date
 }
