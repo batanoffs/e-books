97673e2def9ae9b1c68478367d13d37b998a69b5
batanoffs <batanoff.s@protonmail.com>
2024-08-22 20:08:04 +0300

    add categories handlers routes

--- server/src/routes/admin.ts
+++ server/src/routes/admin.ts
@@ -1,77 +1,118 @@
-import { Router } from 'express';
-import raExpressMongoose from 'express-mongoose-ra-json-server';
+import { Router } from 'express'
+import raExpressMongoose from 'express-mongoose-ra-json-server'
 
-import { isAdmin } from '../middlewares/guards';
-import Book from '../models/Book';
-import User from '../models/User';
-import Order from '../models/Order';
-import Featured from '../models/Featured';
-import Stationery from '../models/Stationery';
-import Textbook from '../models/Textbook';
-import saveCover from '../utils/saveCover';
+import { isAdmin } from '../middlewares/guards'
+import Book from '../models/Book'
+import User from '../models/User'
+import Order from '../models/Order'
+import Featured from '../models/Featured'
+import Stationery from '../models/Stationery'
+import Textbook from '../models/Textbook'
+import saveCover from '../utils/saveCover'
+import Categories from '../models/Category'
 
-const router = Router();
+const router = Router()
 
 //TODO fix admin guard
-router.use(isAdmin);
+router.use(isAdmin)
 
-router.use('/users', raExpressMongoose(User, { q: ['email', 'role'], useLean: false }));
-router.use('/users', raExpressMongoose(User, { q: ['email', 'role'], useLean: false }));
-router.use('/items', raExpressMongoose(Stationery, { q: ['title', 'price', 'description', 'imageUrl', 'stock', 'category'], }));
+router.use('/users', raExpressMongoose(User, { q: ['email', 'role'], useLean: false }))
 
-router.use( '/books',
-    async (req, res, next) => {
-        if (req.method === 'POST' && req.body.cover) {
-            saveCover(req.body, req.body.cover);
-        }
+router.use(
+	'/categories',
+	raExpressMongoose(Categories, {
+		q: ['id', 'books', 'textbooks', 'stationery'],
+		useLean: false,
+	})
+)
+router.use(
+	'/stationery',
+	raExpressMongoose(Stationery, {
+		q: [
+			'title',
+			'price',
+			'description',
+			'coverImage',
+			'coverImageType',
+			'stock',
+			'category',
+			'createdAt',
+		],
+	})
+)
 
-        if (req.method === 'PUT' && req.body.cover) {
-            saveCover(req.body, req.body.cover);
-        }
-        // try {
-        //     const books = await Book.find().lean().exec();
-        //     res.json(books);
-        // } catch (err) {
-        //     next(err);
-        // }
-        next();
-    },
-    raExpressMongoose(Book, {
-        q: [
-            'title',
-            'author',
-            'price',
-            'description',
-            'category',
-            'coverPageType',
-            'cover',
-            'coverImagePath',
-            'dimensions',
-            'language',
-            'pageCount',
-            'publisher',
-            'stock',
-            'translator',
-            'publishDate',
-        ],
-        useLean: true,
-    })
-);
+router.use(
+	'/books',
+	async (req, res, next) => {
+		if (req.method === 'POST' && req.body.cover) {
+			saveCover(req.body, req.body.cover)
+		}
 
-router.use('/textbooks',
-    raExpressMongoose(Textbook, {
-        q: ['title', 'title', 'author', 'price', 'description', 'imageUrl', 'stock', 'category'],
-    })
-);
+		if (req.method === 'PUT' && req.body.cover) {
+			saveCover(req.body, req.body.cover)
+		}
+		// try {
+		//     const books = await Book.find().lean().exec();
+		//     res.json(books);
+		// } catch (err) {
+		//     next(err);
+		// }
+		next()
+	},
+	raExpressMongoose(Book, {
+		q: [
+			'title',
+			'author',
+			'price',
+			'description',
+			'coverImage',
+			'coverImageType',
+			'stock',
+			'categories',
+			'publisher',
+			'language',
+			'publishDate',
+			'pageCount',
+			'translator',
+			'dimensions',
+			'coverPageType',
+			'createdAt',
+		],
+		useLean: true,
+	})
+)
 
-router.use('/featured',
-    raExpressMongoose(Featured, {
-        q: ['title', 'title', 'author', 'price', 'description', 'imageUrl', 'stock', 'category'],
-    })
-);
+router.use(
+	'/textbooks',
+	raExpressMongoose(Textbook, {
+		q: [
+			'title',
+			'author',
+			'price',
+			'description',
+			'coverImage',
+			'coverImageType',
+			'stock',
+			'categories',
+			'publisher',
+			'language',
+			'publishDate',
+			'pageCount',
+			'translator',
+			'dimensions',
+			'coverPageType',
+			'createdAt',
+		],
+	})
+)
 
-router.use('/orders',
-    raExpressMongoose(Order, { q: ['title', 'userId', 'books', 'total', 'status'] })
-);
+router.use(
+	'/featured',
+	raExpressMongoose(Featured, {
+		q: ['title', 'title', 'author', 'price', 'description', 'imageUrl', 'stock', 'category'],
+	})
+)
 
-export default router;
+router.use('/orders', raExpressMongoose(Order, { q: ['userId', 'products', 'total', 'status'] }))
+
+export default router
