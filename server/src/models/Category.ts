a6c336c009e3bc782b1235819993c399e5253724
batanoffs <batanoff.s@protonmail.com>
2024-08-22 20:07:41 +0300

    update category model

--- server/src/models/Category.ts
+++ server/src/models/Category.ts
@@ -3,60 +3,67 @@ import { ICategory } from '../interfaces/category.interface'
 
 const categorySchema = new Schema<ICategory>(
 	{
-		books: {
-			type: [String],
-			unique: true,
-			required: true,
-			trim: true,
-			minlength: 3,
-			lowercase: true,
-		},
-		textbooks: {
-			type: [String],
-			unique: true,
-			required: true,
-			trim: true,
-			minlength: 3,
-			lowercase: true,
-		},
-
-		stationery: {
-			type: [String],
-			unique: true,
-			required: true,
-			trim: true,
-			minlength: 3,
-			lowercase: true,
-		},
+		books: [
+			{
+				type: String,
+				unique: true,
+				required: true,
+				trim: true,
+				minlength: 3,
+				lowercase: true,
+			},
+		],
+		textbooks: [
+			{
+				type: String,
+				unique: true,
+				required: true,
+				trim: true,
+				minlength: 3,
+				lowercase: true,
+			},
+		],
+		stationery: [
+			{
+				type: String,
+				unique: true,
+				required: true,
+				trim: true,
+				minlength: 3,
+				lowercase: true,
+			},
+		],
 	},
 	{
+		versionKey: false,
 		timestamps: true,
+		_id: false,
 	}
 )
 
-const Category = model<ICategory>('Category', categorySchema)
-export default Category
+const Categories = model<ICategory>('Categories', categorySchema)
+export default Categories
 
-const BookCategories = [
-	'Топ заглавия',
-	'Най-нови книги',
-	'Промоции книги',
-	'Детска литература',
-	'Дом, семейство, хоби',
-	'Езотерика',
-	'Енциклопедии',
-	'Изкуство',
-	'Икономика и бизнес',
-	'История и политика',
-	'За тинейджери',
-	'Научно-популярна литература',
-	'Психология и философия',
-	'Речници',
-	'Списания',
-	'Справочници',
-	'Туризъм',
-	'Художествена литература',
-	'Чуждоезикова литература',
-	'Юридическа литература',
-	'Здраве и бизнес',
-]
+// const BookCategories = [
+// 	'Топ заглавия',
+// 	'Най-нови книги',
+// 	'Промоции книги',
+// 	'Детска литература',
+// 	'Дом, семейство, хоби',
+// 	'Езотерика',
+// 	'Енциклопедии',
+// 	'Изкуство',
+// 	'Икономика и бизнес',
+// 	'История и политика',
+// 	'За тинейджери',
+// 	'Научно-популярна литература',
+// 	'Психология и философия',
+// 	'Речници',
+// 	'Списания',
+// 	'Справочници',
+// 	'Туризъм',
+// 	'Художествена литература',
+// 	'Чуждоезикова литература',
+// 	'Юридическа литература',
+// 	'Здраве и бизнес',
+// ]
