2a944cda0d02e8ff04425a87c3f050710733da38
batanoffs <batanoff.s@protonmail.com>
2024-08-27 20:29:27 +0300

    update component format image

--- client/src/pages/ReactAdmin/books/CustomCoverImage.tsx
+++ client/src/pages/ReactAdmin/books/CustomCoverImage.tsx
@@ -1,40 +1,39 @@
-import Typography from '@mui/material/Typography';
-import { useFieldValue } from 'react-admin';
+import Typography from '@mui/material/Typography'
+import { useFieldValue } from 'react-admin'
+import formatImageBase64 from '../../../utils/helpers/formatImageBase64'
 
-type CustomCoverImageProps = {
-    sx?: object;
-    imgWidth?: string;
-    imageTitle?: boolean;
-    newCover?: string;
-    setNewCover?: any;
-    [key: string]: any;
-};
-export const CustomCoverImage = ({
-    sx = {},
-    imgWidth = '100px',
-    imageTitle = false,
-    newCover,
-    setNewCover,
-    ...props
-}: CustomCoverImageProps) => {
-    const coverImage = useFieldValue({ source: 'coverImage' });
-    const coverImageType = useFieldValue({ source: 'coverImageType' });
-    const title = useFieldValue({ source: 'title' });
-    const img = `data:${coverImageType};base64,${coverImage}`;
+type Props = {
+	sx?: object
+	imgWidth?: string
+	imageTitle?: boolean
+	newCover?: string
+}
 
-    if (!coverImage) return null;
+const CustomCoverImage = ({
+	sx = {},
+	imgWidth = '100px',
+	imageTitle = false,
+	newCover,
+	...props
+}: Props) => {
+	const imageData = useFieldValue({ source: 'coverImage' })
+	const imageType = useFieldValue({ source: 'coverImageType' })
+	const title = useFieldValue({ source: 'title' })
+	const img = formatImageBase64(imageType, imageData)
 
-    return (
-        <Typography component="div" sx={sx}>
-            <li style={{ listStyle: 'none' }}>
-                <img
-                    alt="cover"
-                    id='cover'
-                    title={title}
-                    src={newCover ? newCover : img}
-                    style={{ maxWidth: imgWidth }}
-                />
-            </li>
-        </Typography>
-    );
-};
+	return (
+		<Typography component='div' sx={{ ...sx }}>
+			<li style={{ listStyle: 'none' }}>
+				<img
+					alt='Липсва Корица'
+					id='cover'
+					title={imageTitle ? title : undefined}
+					src={img}
+					style={{ overflow: 'hidden', aspectRatio: '2/3', width: imgWidth }}
+				/>
+			</li>
+		</Typography>
+	)
+}
+
+export default CustomCoverImage
