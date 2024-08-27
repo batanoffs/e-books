4ab2ff1f7432e35f1b8aff6e77d5f0bcaa9f2f60
batanoffs <batanoff.s@protonmail.com>
2024-08-27 20:31:13 +0300

    update zustand store set method

--- client/src/store/categories.ts
+++ client/src/store/categories.ts
@@ -1,32 +1,50 @@
 import { create } from 'zustand'
 
-interface CategoryItems {
-	[key: string]: string[]
+interface CategoryItem {
+	_id: string
+	name: string
+	categoryType: string
+	createdAt: string
+	updatedAt: string
 }
 
 interface CategoryStore {
-	categoriesMap: Map<string, string[]>
-	setCategories: (fetchedData: CategoryItems) => void
-	addCategory: (category: string, items: string[]) => void
+	categoriesMap: Map<string, CategoryItem[]>
+	setCategories: (fetchedData: CategoryItem[]) => void
+	addCategory: (category: string, items: CategoryItem[]) => void
 }
 
 const useCategoryStore = create<CategoryStore>((set) => ({
-	categoriesMap: new Map<string, string[]>(),
+	categoriesMap: new Map<string, CategoryItem[]>(),
 
-	setCategories: (fetchedData: CategoryItems) =>
+	setCategories: (fetchedData: CategoryItem[]) =>
 		set(() => {
-			const newCategoryMap = new Map<string, string[]>()
-			const entries = Object.entries(fetchedData)
-			delete entries[0]
-
-			entries.forEach(([category, items]) => {
+			// Create a new Map to hold the categories
+			const newCategoryMap = new Map<string, CategoryItem[]>()
+			// Reduce the fetched data into a nested object structure
+			// where the outer keys are the categories and the values are arrays of items
+			const entries = fetchedData.reduce((accumulator, current) => {
+				// Get the category from the current item
+				const category = current.categoryType
+				// If the category doesn't exist in the accumulator, create it
+				if (!accumulator[category]) {
+					accumulator[category] = []
+				}
+				// Add the current item to the category's array
+				accumulator[category].push(current)
+				return accumulator
+			}, {} as Record<string, CategoryItem[]>)
+			// Loop through the entries and add them to the new category map
+			Object.entries(entries).forEach(([category, items]) => {
+				// Set the category in the new map to the array of items
 				newCategoryMap.set(category, items)
 			})
 
+			// Return the new category map
 			return { categoriesMap: newCategoryMap }
 		}),
 
-	addCategory: (category: string, items: string[]) =>
+	addCategory: (category: string, items: CategoryItem[]) =>
 		set((state) => {
 			state.categoriesMap.set(category, items)
 			return { categoriesMap: new Map(state.categoriesMap) }
