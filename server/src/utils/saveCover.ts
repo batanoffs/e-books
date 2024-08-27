1840f6ca647b8b8e633299c9d5fb112b056b2da0
batanoffs <batanoff.s@protonmail.com>
2024-08-27 20:32:26 +0300

    added check

--- server/src/utils/saveCover.ts
+++ server/src/utils/saveCover.ts
@@ -1,12 +1,13 @@
-import { IBookSchema } from '../interfaces/book.interface';
+import { IBookSchema } from '../interfaces/book.interface'
 
-const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']; // Update if needed
+const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif'] // Update if needed
 
-export default function saveCover(newBook: IBookSchema, coverEncoded: string) {
-    if (!coverEncoded) return;
-    const cover = JSON.parse(coverEncoded);
-    if (cover && imageMimeTypes.includes(cover.type)) {
-        newBook.coverImage = Buffer.from(cover.data, 'base64');
-        newBook.coverImageType = cover.type;
-    }
+export default function saveCover(body: IBookSchema, coverEncoded: string) {
+	if (coverEncoded.length > 0) {
+		const cover = JSON.parse(coverEncoded)
+		if (cover && imageMimeTypes.includes(cover.type)) {
+			body.coverImage = Buffer.from(cover.data, 'base64')
+			body.coverImageType = cover.type
+		}
+	}
 }
