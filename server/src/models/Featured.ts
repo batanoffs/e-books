1a06c3b8cf5bd8ff72a0990bd99d2b758cff5efa
batanoffs <batanoff.s@protonmail.com>
2024-09-01 19:51:16 +0300

    update format for better readability

--- server/src/models/Featured.ts
+++ server/src/models/Featured.ts
@@ -1,12 +1,23 @@
-import { model, Schema } from 'mongoose';
-import { IFeaturedSchema } from '../interfaces/featured.interface';
+import { model, Schema } from 'mongoose'
+import { IFeaturedSchema } from '../interfaces/featured.interface'
 
 const FeaturedSchema: Schema = new Schema({
-    productId: { type: Schema.Types.ObjectId, required: true, refPath: 'productType' },
-    productType: { type: String, required: true, enum: ['Book', 'Textbook', 'Stationery'] },
-    featuredAt: { type: Date, default: Date.now },
-});
+	productId: {
+		type: Schema.Types.ObjectId,
+		required: true,
+		refPath: 'productType',
+	},
+	productType: {
+		type: String,
+		required: true,
+		enum: ['Book', 'Textbook', 'Stationery'],
+	},
+	featuredAt: {
+		type: Date,
+		default: Date.now,
+	},
+})
 
-const Featured = model<IFeaturedSchema>('Featured', FeaturedSchema);
+const Featured = model<IFeaturedSchema>('Featured', FeaturedSchema)
 
-export default Featured;
+export default Featured
