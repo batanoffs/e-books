7aa47920d27fa1d4c9a566442ad515407983c08f
batanoffs <batanoff.s@protonmail.com>
2024-09-01 19:48:40 +0300

    update methods

--- server/src/controllers/categories.ts
+++ server/src/controllers/categories.ts
@@ -1,66 +1,72 @@
 import { Request, Response } from 'express'
-import Categories from '../models/Categories'
 
-//TODO add validation if user is admin, if not reject status 401 Not Authorized
+import { getCategoryModel } from '../utils/checkCategoryType'
 
+/**
+ * Adds a new category item to the database.
+ *
+ * @param {Request} req - Express request object.
+ * @param {Response} res - Express response object.
+ */
 export const addCategory = async (req: Request, res: Response) => {
-	const { categoryType, name } = req.body
+	const {
+		body: { name },
+	} = req
 
-	// Log what the request contains
-	console.log('Received:', { categoryType, name })
+	const categoryType = req.baseUrl.split('/')[3]
+	const categoryModel = getCategoryModel(categoryType)
 
-	if (!['book', 'textbook', 'stationery'].includes(categoryType)) {
-		return res.status(400).json({ message: 'Invalid category type' })
+	if (!name) {
+		return res.status(400).json({ message: 'Name is required' })
 	}
 
 	try {
-		if (!name || !categoryType) {
-			return res.status(400).json({ message: 'Name and categoryType are required' })
-		}
+		const existingCategory = await categoryModel.findOne({ name })
 
-		const existingCategory = await Categories.findOne({ name, categoryType })
 		if (existingCategory) {
 			return res.status(400).json({ message: 'Category already exists' })
 		}
 
-		const category = new Categories({ name, categoryType })
-		await category.save()
-
-		res.json({ message: 'Category added successfully', category })
+		const newCategory = new categoryModel({ name })
+		await newCategory.save()
+		return res.json({ message: 'Category added successfully', newCategory })
 	} catch (error) {
-		console.error('Error adding category:', error)
-		res.status(500).json({ message: 'Error adding category item', error })
-	}
-}
-
-export const getCategoriesByType = async (req: Request, res: Response) => {
-	const { type } = req.params
-
-	// Check if the type is valid
-	if (!['book', 'textbook', 'stationery'].includes(type)) {
-		return res.status(400).json({ message: 'Invalid category type' })
-	}
-
-	try {
-		// Fetch categories by type
-		const categories = await Categories.find({ categoryType: type }).lean().exec()
-
-		if (categories.length > 0) {
-			res.json(categories)
-		} else {
-			res.status(404).json({ message: 'No categories found for this type' })
-		}
-	} catch (error) {
-		res.status(500).json({ message: 'Error fetching categories', error })
+		return res.status(500).json({ message: 'Error adding category item', error })
 	}
 }
 
 export const getAll = async (req: Request, res: Response) => {
+	const categoryType = req.baseUrl.split('/')[3]
+	const categoryModel = getCategoryModel(categoryType)
 	try {
 		// Fetch all categories
-		const categories = await Categories.find().lean().exec()
+		const categories = await categoryModel.find().lean().exec()
 		res.json(categories)
 	} catch (error) {
 		res.status(500).json({ message: 'Error fetching categories', error })
 	}
 }
+
+//TODO add validation if user is admin, if not reject status 401 Not Authorized
+
+// export const getCategoriesByType = async (req: Request, res: Response) => {
+// 	const { type } = req.params
+
+// 	// Check if the type is valid
+// 	if (!['book', 'textbook', 'stationery'].includes(type)) {
+// 		return res.status(400).json({ message: 'Invalid category type' })
+// 	}
+
+// 	try {
+// 		// Fetch categories by type
+// 		const categories = await BookCategories.find({ categoryType: type }).lean().exec()
+
+// 		if (categories.length > 0) {
+// 			res.json(categories)
+// 		} else {
+// 			res.status(404).json({ message: 'No categories found for this type' })
+// 		}
+// 	} catch (error) {
+// 		res.status(500).json({ message: 'Error fetching categories', error })
+// 	}
+// }
