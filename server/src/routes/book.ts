eecd1b71a8897f0b5a7ac8546d54235603090a2e
batanoffs <batanoff.s@protonmail.com>
2024-09-02 23:49:06 +0300

    add upload route

--- server/src/routes/book.ts
+++ server/src/routes/book.ts
@@ -1,38 +1,36 @@
-import { Router } from 'express';
-import {
-    createBook,
-    getBooks,
-    getBookById,
-    deleteBook,
-} from '../controllers/book';
-import { body } from 'express-validator';
-import validateRequest from '../middlewares/validateRequest';
+import { Router } from 'express'
+import { body } from 'express-validator'
 
-const router = Router();
+import { createBook, getBooks, getBookById, deleteBook } from '../controllers/book'
+import validateRequest from '../middlewares/validateRequest'
+import upload from '../middlewares/multer'
+import { uploadCoverImage } from '../controllers/images'
+const router = Router()
 
 router.post(
-    '/',
-    [
-        body('title').notEmpty().isLength({ min: 2 }).withMessage('Title is required'),
-        body('author').notEmpty().withMessage('Author is required'),
-        body('price').isFloat({ gt: 0 }).withMessage('Price must be a positive number'),
-        body('description').notEmpty().isLength({ min: 10 }).withMessage('Description min 10 characters'),
-        body('categories').notEmpty().isLength({ min: 3 }).withMessage('Categories are required'),
-        body('coverPageType').notEmpty().withMessage('Cover page type is required'),
-        body('dimensions').isLength({ min: 1 }).withMessage('Dimensions is required'),
-        body('language').notEmpty().withMessage('Language is required'),
-        body('pageCount').isNumeric().withMessage('Pages must be a positive integer'),
-        body('publisher').notEmpty().withMessage('Publisher is required'),
-        body('stock').isInt({ gt: 0 }).withMessage('Stock must be a positive integer'),
-        body('translator').notEmpty().withMessage('Translator is required'),
-        body('publishDate').isDate().withMessage('Year published is required'),
-        validateRequest,
-    ],
-    createBook
-);
+	'/',
+	[
+		body('title').notEmpty().isLength({ min: 2 }).withMessage('Title is required'),
+		body('author').notEmpty().withMessage('Author is required'),
+		body('price').isFloat({ gt: 0 }).withMessage('Price must be a positive number'),
+		body('description').notEmpty().isLength({ min: 10 }).withMessage('Description min 10 characters'),
+		body('categories').notEmpty().isLength({ min: 3 }).withMessage('Categories are required'),
+		body('coverPageType').notEmpty().withMessage('Cover page type is required'),
+		body('dimensions').isLength({ min: 1 }).withMessage('Dimensions is required'),
+		body('language').notEmpty().withMessage('Language is required'),
+		body('pageCount').isNumeric().withMessage('Pages must be a positive integer'),
+		body('publisher').notEmpty().withMessage('Publisher is required'),
+		body('stock').isInt({ gt: 0 }).withMessage('Stock must be a positive integer'),
+		body('translator').notEmpty().withMessage('Translator is required'),
+		body('publishDate').isDate().withMessage('Year published is required'),
+		validateRequest,
+	],
+	createBook
+)
 
-router.get('/', getBooks);
-router.get('/:id', getBookById);
-router.delete('/:id', deleteBook);
+router.get('/', getBooks)
+router.get('/:id', getBookById)
+router.delete('/:id', deleteBook)
+router.post('/upload', upload.single('picture'), uploadCoverImage)
 
-export default router;
+export default router
