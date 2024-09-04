75ce5498c3aa8a67ddcd66720eb46141cf9627d8
batanoffs <batanoff.s@protonmail.com>
2024-09-04 15:37:53 +0300

    add route to fetch all categories at once

--- server/src/routes/categories.ts
+++ server/src/routes/categories.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 import categoryType from './categoriesType'
+import { fetchAllTypesOfCategories } from '../controllers/categories'
 
 const router = Router()
 
 router.use('/books', categoryType)
 router.use('/textbooks', categoryType)
 router.use('/stationery', categoryType)
+
+router.get('/', fetchAllTypesOfCategories)
 
 export default router
