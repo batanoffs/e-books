0ea68b342e2a64f30cceb1934048c4a2282ef119
batanoffs <batanoff.s@protonmail.com>
2024-09-04 15:27:35 +0300

    added new component for categories carousels on homepage

---
+++ client/src/components/Carousels/CategoriesCarousel.tsx
@@ -0,0 +1,36 @@
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import { CategoryItem } from '../../interfaces/categories.interface'
+
+interface CategoriesCarouselInterface {
+	categories: CategoryItem[]
+	Component: React.ComponentType<React.PropsWithChildren<{ category: CategoryItem }>>
+	sx?: React.CSSProperties
+}
+
+const CategoriesCarousel = ({ categories, Component, sx = {} }: CategoriesCarouselInterface) => {
+	const settings = {
+		className: 'centerPadding',
+		swipeToSlide: true,
+		infinite: true,
+		autoplay: false,
+		dots: false,
+		centerPadding: '0px',
+		slidesToScroll: 5,
+		slidesToShow: 5,
+		speed: 500,
+		rows: 1,
+	}
+
+	return (
+		<Slider {...settings}>
+			{categories &&
+				categories.map((category: CategoryItem) => (
+					<Component key={category._id} category={category} />
+				))}
+		</Slider>
+	)
+}
+
+export default CategoriesCarousel
