e7bcfe7aad144100599a32132972fb6d97a5836e
batanoffs <batanoff.s@protonmail.com>
2024-09-04 15:28:29 +0300

    update imports and params for fetched data

--- client/src/components/Categories/Categories.tsx
+++ client/src/components/Categories/Categories.tsx
@@ -1,14 +1,14 @@
 import { useCallback, useEffect } from 'react'
+import Typography from '@mui/material/Typography'
 import axios from 'axios'
 
-import Typography from '@mui/material/Typography'
+import CategoriesCarousel from '../Carousels/CategoriesCarousel'
 import useSpinner from '../../store/spinner'
 import useCategoryStore from '../../store/categories'
-import MultiCarousel from '../Carousels/MultiCarousel'
 import CategoryItem from './categoryItem'
 import API from '../../utils/constants/api'
 
-import styles from './categories.module.scss' // Import the SCSS module
+import styles from './categories.module.scss'
 
 const CategoryList = () => {
 	const setCategories = useCategoryStore((state) => state.setCategories)
@@ -18,8 +18,6 @@ const CategoryList = () => {
 	const fetchBookCategoriesCallback = useCallback(async () => {
 		try {
 			const response = await axios.get(API.CATEGORIES)
-			console.log(response.data)
-
 			setCategories(response.data)
 		} catch (error) {
 			console.error(error)
@@ -31,14 +29,6 @@ const CategoryList = () => {
 	useEffect(() => {
 		fetchBookCategoriesCallback()
 	}, [fetchBookCategoriesCallback])
-	console.log('categoriesMap', categoriesMap)
-
-	const bookCategories = categoriesMap.get('book')
-	const textbooksCategories = categoriesMap.get('textbook')
-	const stationeryCategories = categoriesMap.get('stationery')
-
-	console.log('bookCategories', bookCategories);
-	
 
 	return (
 		<div className={styles.wrapper}>
@@ -49,20 +39,23 @@ const CategoryList = () => {
 				<Typography variant='h5' className={styles.categoryTitle}>
 					Категории "Книги"
 				</Typography>
-				<MultiCarousel items={bookCategories} CardComponent={CategoryItem} />
+				<CategoriesCarousel categories={categoriesMap.books} Component={CategoryItem} />
 			</div>
 			<div className={styles.categoryContainer}>
 				<Typography variant='h5' className={styles.categoryTitle}>
 					Категории "Учебници"
 				</Typography>
-				<MultiCarousel items={textbooksCategories} CardComponent={CategoryItem} />
+				<CategoriesCarousel categories={categoriesMap.textbooks} Component={CategoryItem} />
 			</div>
 
 			<div className={styles.categoryContainer}>
 				<Typography variant='h5' className={styles.categoryTitle}>
 					Категории "Канцелария"
 				</Typography>
-				<MultiCarousel items={stationeryCategories} CardComponent={CategoryItem} />
+				<CategoriesCarousel
+					categories={categoriesMap.stationeries}
+					Component={CategoryItem}
+				/>
 			</div>
 		</div>
 	)
