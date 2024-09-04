b780f120ccb269069c54df09c7cf0c991e696613
batanoffs <batanoff.s@protonmail.com>
2024-09-04 15:30:48 +0300

    create products interface

---
+++ client/src/interfaces/product.interface.ts
@@ -0,0 +1,54 @@
+export interface Book {
+	_id: string
+	title: string
+	author: string
+	price: number
+	description: string
+	picture: string
+	coverPageType: string
+	stock: number
+	categories: string[]
+	publisher?: string
+	language?: string
+	publishDate?: Date
+	pageCount?: number
+	translator?: string
+	dimensions?: string
+	createdAt: Date
+}
+
+export interface Textbook {
+	_id: string
+	title: string
+	author: string
+	price: number
+	description: string
+	picture: string
+	coverPageType: string
+	stock: number
+	categories: string[]
+	publisher?: string
+	language?: string
+	publishDate?: Date
+	pageCount?: number
+	translator?: string
+	dimensions?: string
+	createdAt: Date
+}
+
+export interface Stationery {
+	_id: string
+	title: string
+	price: number
+	description: string
+	picture: string
+	categories: string[]
+	stock: number
+	createdAt: Date
+}
+
+export type Products = {
+	products: Book[] | Textbook[] | Stationery[]
+}
+
+export type Product = Book | Textbook | Stationery
