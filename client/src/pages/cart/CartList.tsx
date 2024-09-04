9dbf9eec85766e8a0ce2d006bc55980706c02151
batanoffs <batanoff.s@protonmail.com>
2024-09-04 15:31:45 +0300

    update component handlers

--- client/src/pages/cart/CartList.tsx
+++ client/src/pages/cart/CartList.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Paper from '@mui/material/Paper'
@@ -11,46 +10,53 @@ import useCartStore from '../../store/cart'
 import useConfirm from '../../hooks/useConfirm'
 import cartService from '../../services/cartService'
 import authService from '../../services/authService'
+import useAlertStore from '../../store/alert'
 
 const CartList = () => {
 	const [isUpdateButtonDisabled, setIsUpdateButtonDisabled] = useState(true)
-	const { dialog, confirm } = useConfirm()
-
 	const updateQuantity = useCartStore((state) => state.updateQuantity)
 	const clearCart = useCartStore((state) => state.clearCart)
+	const showAlert = useAlertStore((state) => state.showAlert)
 	const cart = useCartStore((state) => state.cart)
+	const { dialog, confirm } = useConfirm()
 
 	const getTotalItems = () => cart.reduce((sum, item) => sum + item.quantity, 0)
 
 	const clearCartHandler = async () => {
-		const userId = await authService.getUserId()
-		const result = await confirm(
-			'Изчистане на всички продукти',
-			'Сигурни ли сте, че искате да изчистите количката?'
-		)
-		if (result) {
-			await cartService.removeAll(userId)
-			clearCart()
+		try {
+			const userId = await authService.getUserId()
+			const result = await confirm(
+				'ВНИМАНИЕ! Изчистване на всички продукти от количката!',
+				'Сигурни ли сте, че искате да изтриете всички продукти от количката?'
+			)
+			if (result) {
+				await cartService.removeAll(userId)
+				clearCart()
+				showAlert(`Количката е изчитена успешно`, 'success')
+			}
+		} catch (error) {
+			showAlert(`Грешка при изчистването на количката ${error}`, 'error')
 		}
 	}
 
+	//TODO finish logic for update cart
 	const updateCartHandler = async (e) => {
 		console.log('event:', e.target)
-		const inputs = e.target.parentElement.parentElement.querySelectorAll('input')
-		const values = []
-		inputs.forEach((input) => values.push(input.value))
-		console.log('inputs:', inputs)
-		console.log('values:', values)
+		// const inputs = e.target.parentElement.parentElement.querySelectorAll('input')
+		// const values = []
+		// inputs.forEach((input) => values.push(input.value))
+		// console.log('inputs:', inputs)
+		// console.log('values:', values)
 
-		const promises = cart.map((item) =>
-			updateQuantity(item.product.id, values[cart.indexOf(item)])
-		)
-		const response = await Promise.all(promises)
+		// const promises = cart.map((item) =>
+		// 	updateQuantity(item.product._id, values[cart.indexOf(item)])
+		// )
+		// const response = await Promise.all(promises)
 
-		if (response) setIsUpdateButtonDisabled(true)
-		if (response) {
-			//TODO updated backend quantity of the current items in the cart
-		}
+		// if (response) setIsUpdateButtonDisabled(true)
+		// if (response) {
+		// 	//TODO updated backend quantity of the current items in the cart
+		// }
 	}
 
 	const onChangeQuantityHandler = () => {
@@ -72,8 +78,12 @@ const CartList = () => {
 			) : (
 				<Grid container spacing={0}>
 					{cart.map((item) => (
-						<Grid item xs={12} key={item.product.id}>
-							<CartItem item={item} onChangeQuantity={onChangeQuantityHandler} />
+						<Grid item xs={12} key={item.product._id}>
+							<CartItem
+								product={item.product}
+								quantity={item.quantity}
+								onChangeQuantity={onChangeQuantityHandler}
+							/>
 						</Grid>
 					))}
 
