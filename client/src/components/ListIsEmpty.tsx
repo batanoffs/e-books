d9b2be39b38501854e8a42ecbd2c2978def675ae
batanoffs <batanoff.s@protonmail.com>
2024-09-16 17:38:31 +0300

    add custom component empty list util

---
+++ client/src/components/ListIsEmpty.tsx
@@ -0,0 +1,35 @@
+import { useNavigate } from 'react-router-dom'
+
+import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+
+export const ListIsEmpty = () => {
+	const navigate = useNavigate()
+
+	return (
+		<Box
+			sx={{
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+				justifyContent: 'center',
+				height: '40vh',
+				textAlign: 'center',
+			}}
+		>
+			<Typography variant='h3' sx={{ mb: 2 }}>
+				Няма намерени продукти
+			</Typography>
+			<Typography variant='subtitle1'>Добави продукт и той ще се появи тук</Typography>
+			<Button
+				variant='contained'
+				onClick={() => navigate(`/catalog/books/all`)}
+				color='primary'
+				sx={{ mt: 4 }}
+			>
+				Обратно към каталога
+			</Button>
+		</Box>
+	)
+}
