071c0a07bea42c67899119e66ee1c6e77f811961
batanoffs <batanoff.s@protonmail.com>
2024-09-17 18:58:48 +0300

    update handlers and props

--- client/src/pages/checkout/CheckoutPage.tsx
+++ client/src/pages/checkout/CheckoutPage.tsx
@@ -6,6 +6,9 @@ import { CheckoutOverview } from './CheckoutAsideOverview'
 import { DeliveryForm } from './DeliveryForm'
 import { AddressForm } from './AddressForm'
 import API from '../../utils/constants/api'
+import axios from 'axios'
+import useCartStore from '../../store/cart'
+import { useEffect } from 'react'
 
 // import { PaymentForm } from './PaymentForm'
 // import { DiscountCode } from './DiscountCode'
@@ -16,38 +19,38 @@ import API from '../../utils/constants/api'
 type CheckoutFormValues = {
 	firstName: string
 	lastName: string
-	telephone: string
+	telephone: number
 	postcode: string
-	region_id: string
+	regionId: number
 	city: string
-	street: string
+	address: string
 	shippingMethod: string
 	paymentMethod: string
 	discountCode: string
 	comment: string
-	custom_attributes: {
-		shipping_delivery_data: string
+	customAttributes: {
+		deliveryData: string
 	}
 }
 let renderTime = 0
 
 const CheckoutPage = () => {
 	// const placeOrder = useCartStore((state) => state.placeOrder)
-	const { control, handleSubmit, formState, reset, register } = useForm<CheckoutFormValues>({
+	const cart = useCartStore((state) => state.cart)
+
+	const { control, handleSubmit, formState, reset, register } = useForm({
 		defaultValues: {
 			firstName: '',
 			lastName: '',
 			telephone: '',
-			region_id: '',
+			regionId: '',
 			city: '',
-			street: '',
+			address: '',
 			shippingMethod: '',
-			paymentMethod: '',
 			discountCode: '',
-			comment: '',
 			postcode: '',
-			custom_attributes: {
-				shipping_delivery_data: '',
+			customAttributes: {
+				deliveryData: '',
 			},
 		},
 	})
@@ -56,33 +59,56 @@ const CheckoutPage = () => {
 
 	const { errors } = formState // Extract the errors from formState
 
-	const handlePlaceOrder = (data: CheckoutFormValues) => {
-		console.table(data)
+	const handlePlaceOrder = async (input: CheckoutFormValues) => {
+		console.table(input)
 		console.log('formState', formState)
+
 		// const token = getToken()
-		fetch(API.CHECKOUT + 'create-checkout-session', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				// 'Authorization': `Bearer: ${token}`,
-			},
-			body: JSON.stringify({
-				items: [
-					{ id: 1, quantity: 1 },
-					{ id: 2, quantity: 2 },
-				],
-			}),
+		const products = cart.map((item) => {
+			return {
+				id: item.product._id,
+				quantity: item.quantity,
+			}
 		})
-			.then((res) => {
-				if (res.ok) return res.json()
-				return res.json().then((json) => Promise.reject(json))
-			})
-			.then(({ url }) => {
-				window.location = url
-			})
-			.catch((error) => {
-				console.error(error.error)
-			})
+		const data = {
+			products,
+		}
+		try {
+			const response = await axios.post(API.CHECKOUT + 'create-checkout-session', data)
+			if (response.status === 400) return Promise.reject(response)
+			if (response.status === 401) return Promise.reject(response)
+			if (response.status === 500) return Promise.reject(response)
+			if (response.status === 200) {
+				console.log('response', response.data)
+				window.location = response.data.url
+			}
+		} catch (error) {
+			console.error(error)
+		}
+
+		// fetch(API.CHECKOUT + 'create-checkout-session', {
+		// 	method: 'POST',
+		// 	headers: {
+		// 		'Content-Type': 'application/json',
+		// 		// 'Authorization': `Bearer: ${token}`,
+		// 	},
+		// 	body: JSON.stringify({
+		// 		items: [
+		// 			{ id: 1, quantity: 1 },
+		// 			{ id: 2, quantity: 2 },
+		// 		],
+		// 	}),
+		// })
+		// 	.then((res) => {
+		// 		if (res.ok) return res.json()
+		// 		return res.json().then((json) => Promise.reject(json))
+		// 	})
+		// 	.then(({ url }) => {
+		// 		window.location = url
+		// 	})
+		// 	.catch((error) => {
+		// 		console.error(error.error)
+		// 	})
 	}
 
 	const handleBackToCart = () => {
@@ -95,7 +121,7 @@ const CheckoutPage = () => {
 			onSubmitForm={handleSubmit(handlePlaceOrder)}
 			control={control}
 		>
-			<DeliveryForm errors={errors} control={control} />
+			<DeliveryForm errors={errors} control={control} register={register} />
 			<AddressForm errors={errors} control={control} register={register} />
 			{/* <PaymentForm control={control} errors={errors} /> */}
 			{/* <DiscountCode control={control} reset={reset} errors={errors} /> */}
