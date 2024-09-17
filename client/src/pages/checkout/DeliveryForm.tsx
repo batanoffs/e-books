77b7b3d0b43278ca5307fb76c2e0e38aa3cc27a0
batanoffs <batanoff.s@protonmail.com>
2024-09-17 18:59:09 +0300

    refactor component items

--- client/src/pages/checkout/DeliveryForm.tsx
+++ client/src/pages/checkout/DeliveryForm.tsx
@@ -1,64 +1,33 @@
 import Box from '@mui/material/Box'
 import FormControl from '@mui/material/FormControl'
-import FormControlLabel from '@mui/material/FormControlLabel'
-import Radio from '@mui/material/Radio'
-import RadioGroup from '@mui/material/RadioGroup'
-import Table from '@mui/material/Table'
-import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
-import TableRow from '@mui/material/TableRow'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import Select from '@mui/material/Select'
 import Typography from '@mui/material/Typography'
-import { Controller } from 'react-hook-form'
+
 import { deliveryOptions } from '../../utils/constants/paymentFormConstants'
-import { TableHead } from '@mui/material'
 
-//TODO Provide options for office and onsite delivery!!!
-
-export const DeliveryForm = ({ errors, control }) => {
+export const DeliveryForm = ({ register, errors, control }) => {
 	return (
 		<Box component='div' id='shipping-method'>
-			<FormControl component='fieldset' fullWidth>
-				<Typography variant='h5' sx={{ marginBottom: '2rem' }}>
-					Изберете начин на доставка
-				</Typography>
-				<Controller
+			<Typography variant='h5' sx={{ marginBottom: '1rem' }}>
+				Изберете начин на доставка
+			</Typography>
+			<FormControl fullWidth error={!!errors.shippingMethod}>
+				<InputLabel id='shipping-method-label'>Доставчик</InputLabel>
+				<Select
+					labelId='shipping-method-label'
+					id='shipping-method'
 					name='shippingMethod'
-					control={control}
-					rules={{ required: 'Please select a shipping method' }}
-					render={({ field }) => (
-						<RadioGroup {...field}>
-							<Table>
-								<TableHead>
-									<TableRow>
-										<TableCell></TableCell>
-										<TableCell>Доставчик</TableCell>
-										<TableCell>Метод</TableCell>
-										<TableCell>Цена</TableCell>
-									</TableRow>
-								</TableHead>
-								<TableBody>
-									{deliveryOptions.map((option) => (
-										<TableRow key={option.value}>
-											<TableCell>
-												<FormControlLabel
-													value={option.value}
-													control={<Radio />}
-													label={''}
-												/>
-											</TableCell>
-											<TableCell>{option.method}</TableCell>
-											<TableCell>{option.provider}</TableCell>
-											<TableCell>{option.price}</TableCell>
-										</TableRow>
-									))}
-								</TableBody>
-							</Table>
-						</RadioGroup>
-					)}
-				/>
-				{errors?.shippingMethod && (
-					<Typography color='error'>{errors.shippingMethod.message}</Typography>
-				)}
+					label='Shipping method'
+					{...register('shippingMethod')} //  { required: 'Полето е задължителено' }
+				>
+					{deliveryOptions.map(({ value, provider }) => (
+						<MenuItem key={value} value={value}>
+							{provider}
+						</MenuItem>
+					))}
+				</Select>
 			</FormControl>
 		</Box>
 	)
