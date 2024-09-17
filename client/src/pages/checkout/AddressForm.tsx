b013cc6a6d4a2c8506b67f143c91351732216445
batanoffs <batanoff.s@protonmail.com>
2024-09-17 18:58:19 +0300

    refactore component items

--- client/src/pages/checkout/AddressForm.tsx
+++ client/src/pages/checkout/AddressForm.tsx
@@ -6,68 +6,64 @@ import MenuItem from '@mui/material/MenuItem'
 import Select from '@mui/material/Select'
 import { Controller } from 'react-hook-form'
 import { regionOptions } from '../../utils/constants/paymentFormConstants'
-import { InputFormField } from '../../components/InputFormField/InputFormField'
-import { Typography } from '@mui/material'
+import { TextField, Typography } from '@mui/material'
+// import { InputFormField } from '../../components/InputFormField/InputFormField'
 
 export const AddressForm = ({ control, errors, register }) => {
 	return (
 		<Box component='div' id='address-form'>
 			<Typography variant='h5'>Адрес за доставка</Typography>
 
-			<Grid container sx={{ color: 'red', columnGap: '4em' }}>
-				<Grid item xs={12} sm={5}>
-					<InputFormField
+			<Grid container sx={{ color: 'red' }}>
+				<Grid item xs={12} sm={6}>
+					<TextField
 						label='Име'
 						name='firstName'
 						type='text'
-						required
-						control={control}
-						{...register('firstName')}
-						errors={errors?.firstName}
+						{...register('firstName')} //{ required: 'Полето е задължителено' }
+						error={!!errors.firstName}
+						helperText={errors.firstName?.message}
 					/>
 				</Grid>
-				<Grid item xs={12} sm={5}>
-					<InputFormField
+				<Grid item xs={12} sm={6}>
+					<TextField
 						label='Фамилия'
 						name='lastName'
 						type='text'
-						required
-						control={control}
-						{...register('lastName')}
-						errors={errors?.lastName}
+						{...register('lastName')} //{ required: 'Полето е задължителено' }
+						error={!!errors.lastName}
+						helperText={errors.lastName?.message}
 					/>
 				</Grid>
-				<Grid item xs={12} sm={5}>
-					<InputFormField
+				<Grid item xs={12} sm={6}>
+					<TextField
 						label='Телефон'
 						type='number'
 						name='telephone'
-						required
-						control={control}
-						{...register('telephone')}
-						errors={errors?.telephone}
+						{...register('telephone')} //{ required: 'Полето е задължителено' }
+						error={!!errors.telephone}
+						helperText={errors.telephone?.message}
 					/>
 				</Grid>
-				<Grid item xs={12} sm={5}>
-					<InputFormField
+				<Grid item xs={12} sm={6}>
+					<TextField
 						label='Пощенски код'
 						type='number'
 						name='postcode'
-						control={control}
-						{...register('postcode')}
-						errors={errors?.postcode}
+						{...register('postcode')} //{ required: 'Полето е задължителено' }
+						error={!!errors.postcode}
+						helperText={errors.postcode?.message}
 					/>
 				</Grid>
-				<Grid item xs={12} sm={5}>
-					<FormControl fullWidth margin='normal'>
+				<Grid item xs={12} sm={6}>
+					<FormControl fullWidth margin='normal' error={!!errors.postcode}>
 						<InputLabel>Област / Провинция</InputLabel>
 						<Controller
-							name='region_id'
-							type='text'
+							name='regionId'
 							control={control}
-							{...register('region_id')}
+							{...register('regionId')} //{ required: 'Полето е задължителено' }
 							render={({ field }) => (
-								<Select {...field} required>
+								<Select {...field}>
 									{regionOptions.map((option) => (
 										<MenuItem key={option.value} value={option.value}>
 											{option.label}
@@ -78,38 +74,37 @@ export const AddressForm = ({ control, errors, register }) => {
 						/>
 					</FormControl>
 				</Grid>
-				<Grid item xs={12} sm={5}>
-					<InputFormField
+				<Grid item xs={12} sm={6}>
+					<TextField
 						label='Населено място'
 						name='city'
 						type='text'
-						required
-						control={control}
-						{...register('city')}
-						errors={errors?.city}
+						{...register('city')} //{ required: 'Полето е задължителено' }
+						error={!!errors.city}
+						helperText={errors.city?.message}
 					/>
 				</Grid>
-				<Grid item xs={11}>
-					<InputFormField
+				<Grid item xs={12}>
+					<TextField
 						label='Адрес'
-						name='street'
+						name='address'
 						type='text'
-						required
-						control={control}
-						{...register('street')}
-						errors={errors?.street}
+						{...register('address')} //{ required: 'Полето е задължителено' }
+						error={!!errors.address}
+						helperText={errors.address?.message}
 					/>
 				</Grid>
-				<Grid item xs={11}>
-					<InputFormField
+				<Grid item xs={12}>
+					<TextField
 						label='Допълнителна информация за доставка'
-						name='custom_attributes.shipping_delivery_data'
+						name='deliveryData'
+						id='deliveryData'
 						multiline={true}
 						type='text'
 						rows={3}
-						control={control}
-						{...register('custom_attributes.shipping_delivery_data')}
-						errors={errors?.street}
+						{...register('deliveryData')} //{ required: 'Полето е задължителено' }
+						error={!!errors.customAttributes}
+						helperText={errors.customAttributes?.message}
 					/>
 				</Grid>
 			</Grid>
