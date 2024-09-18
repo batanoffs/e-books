c116bcfecc3dd24ed05e1f02ea8c4eecec03c0c8
batanoffs <batanoff.s@protonmail.com>
2024-09-18 19:33:33 +0300

    add get route

--- server/src/routes/stripe.ts
+++ server/src/routes/stripe.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express'
-import { checkoutSession } from '../controllers/stripe'
+import { checkoutSession, getPaymentSessionAndCreateOrder } from '../controllers/stripe'
 
 const router = Router()
 
 router.post('/create-checkout-session', checkoutSession)
-
-// router.get('/session-status', getPaymentSessionStatus)
+router.get('/session-status', getPaymentSessionAndCreateOrder)
 
 export default router
