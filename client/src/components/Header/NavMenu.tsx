09426bf69cba8c48010ff5921c08f56b9b67fe61
batanoffs <batanoff.s@protonmail.com>
2024-09-22 13:29:07 +0300

    remove menu item

--- client/src/components/Header/NavMenu.tsx
+++ client/src/components/Header/NavMenu.tsx
@@ -6,7 +6,7 @@ import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import MenuIcon from '@mui/icons-material/Menu'
 
-const pages = ['Най-продавани', 'Книги', 'Учебници', 'Канцелария']
+const pages = ['Книги', 'Учебници', 'Канцелария']
 
 const NavigationMenu = ({
 	anchorElNav,
