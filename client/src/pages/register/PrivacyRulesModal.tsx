95ccac3a52316e9b527f6cb657c795149402403c
batanoffs <batanoff.s@protonmail.com>
2024-09-22 12:36:16 +0300

    fix type issues

--- client/src/pages/register/PrivacyRulesModal.tsx
+++ client/src/pages/register/PrivacyRulesModal.tsx
@@ -1,38 +1,39 @@
 import Modal from '@mui/material/Modal'
 import { usePrivacyModal } from '../../store/helperModal'
+import { Box } from '@mui/material'
 
 const PrivacyRulesModal = () => {
 	const open = usePrivacyModal((state) => state.open)
 	const toggleOpen = usePrivacyModal((state) => state.toggleOpen)
 
-	const styles = {
-		display: 'flex',
-		flexDirection: 'column',
-		alignItems: 'center',
-		justifyContent: 'center',
-		padding: '20px',
-		backgroundColor: '#fff',
-		borderRadius: '0.3125em',
-		boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
-		position: 'fixed',
-		top: '50%',
-		left: '50%',
-		transform: 'translate(-50%, -50%)',
-	}
-
 	return (
 		<Modal open={open} onClose={toggleOpen}>
-			<div style={styles}>
+			<Box
+				sx={{
+					display: 'flex',
+					alignItems: 'center',
+					flexDirection: 'column',
+					justifyContent: 'center',
+					padding: '20px',
+					backgroundColor: '#fff',
+					borderRadius: '0.3125em',
+					boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+					position: 'fixed',
+					top: '50%',
+					left: '50%',
+					transform: 'translate(-50%, -50%)',
+				}}
+			>
 				<h2>Политика за поверителност</h2>
 				<p>
-					Това са политиките за поверителност, които сте се запознали с. Ще събираме
-					вашата информация за целите на нашата платформа. Вашите данни не бяха
-					предоставени на трети страни. Ще ги ползваме само за целите на нашата платформа.
+					Целта на тази политика за поверителност е да Ви информира за начина, по който
+					ние колективно събираме, използваме и разкриваме информация за вас при
+					използване на нашата платформа. Ние се ангажираме да защитаваме вашата
+					поверителност и да я използваме само за целите, за които е предназначена.
 				</p>
-			</div>
+			</Box>
 		</Modal>
 	)
 }
 
 export default PrivacyRulesModal
-
