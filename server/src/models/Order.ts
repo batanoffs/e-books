b23608e411dbd01605e465267e0d40b1af83e628
batanoffs <batanoff.s@protonmail.com>
2024-09-24 20:28:11 +0300

    update order model

--- server/src/models/Order.ts
+++ server/src/models/Order.ts
@@ -1,42 +1,44 @@
 import { model, Schema } from 'mongoose'
 import { IOrderSchema } from '../interfaces/order.interface'
 
-const OrderSchema: Schema = new Schema({
-	userId: {
-		type: String,
-		required: true,
-	},
-	products: [
-		{
-			productId: {
-				type: Schema.Types.ObjectId,
-				required: true,
-				refPath: 'productType',
-			},
-			productType: {
-				type: String,
-				required: true,
-				enum: ['Book', 'Textbook', 'Stationery'],
-			},
-			quantity: { type: Number, required: true },
+const OrderSchema: Schema = new Schema(
+	{
+		userId: {
+			type: String,
+			required: true,
 		},
-	],
-	total: {
-		type: Number,
-		required: true,
+		products: [
+			{
+				productType: {
+					type: String,
+					required: true,
+					enum: ['Book', 'Textbook', 'Stationery'],
+				},
+				productId: {
+					type: Schema.Types.ObjectId,
+					required: true,
+					refPath: 'products.productType',
+				},
+				quantity: { type: Number, required: true },
+			},
+		],
+		total: {
+			type: Number,
+			required: true,
+		},
+		shippingStatus: {
+			type: String,
+			required: true,
+			enum: ['pending', 'shipped', 'delivered'],
+			default: 'pending',
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+		},
 	},
-	orderStatus: {
-		type: String,
-		required: true,
-		default: 'pending',
-	},
-	shippingStatus: {
-		type: String,
-		required: true,
-		enum: ['pending', 'shipped', 'delivered'],
-		default: 'pending',
-	},
-})
+	{ timestamps: true }
+)
 
 const Order = model<IOrderSchema>('Order', OrderSchema)
 
