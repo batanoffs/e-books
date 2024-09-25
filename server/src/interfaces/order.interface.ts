fc4e81c5ec4436b82ee276d0eed4be9e21524868
batanoffs <batanoff.s@protonmail.com>
2024-09-25 20:59:50 +0300

    update order model and interface

--- server/src/interfaces/order.interface.ts
+++ server/src/interfaces/order.interface.ts
@@ -1,7 +1,7 @@
-import { Types } from 'mongoose'
+import { Document, Types } from 'mongoose'
 
 export interface IOrderSchema extends Document {
-	userId: String
+	userId: string
 	products: {
 		productId: Types.ObjectId
 		productType: 'Book' | 'Textbook' | 'Stationery'
@@ -9,5 +9,18 @@ export interface IOrderSchema extends Document {
 	}[]
 	total: number
 	shippingStatus: 'pending' | 'shipped' | 'delivered'
+	shippingProvider?: string
+	shipping_details: {
+		address: {
+			city: string
+			country: string
+			line1: string
+			line2?: string | null
+			postal_code?: string | null
+			state?: string | null
+		}
+		name: string
+	}
+	paymentStatus: 'pending' | 'paid' | 'refunded'
 	createdAt: Date
 }
--- server/src/models/Order.ts
+++ server/src/models/Order.ts
@@ -32,12 +32,81 @@ const OrderSchema: Schema = new Schema(
 			enum: ['pending', 'shipped', 'delivered'],
 			default: 'pending',
 		},
+		shippingProvider: {
+			type: String,
+			trim: true,
+			enum: ['Speedy', 'Econt'],
+		},
+		customer_details: {
+			type: {
+				address: {
+					type: {
+						city: String,
+						country: String,
+						line1: String,
+						line2: String,
+						postal_code: String,
+						state: String,
+					},
+				},
+				email: String,
+				name: String,
+				phone: String,
+				tax_exempt: String,
+				tax_ids: [{ type: String }],
+			},
+			default: {},
+		},
+		shipping_details: {
+			address: {
+				city: {
+					type: String,
+					required: true,
+				},
+				country: {
+					type: String,
+					required: true,
+				},
+				line1: {
+					type: String,
+					required: true,
+				},
+
+				line2: {
+					type: String || null,
+					required: false,
+				},
+				postal_code: {
+					type: String || null,
+					required: false,
+				},
+				state: {
+					type: String || null,
+					required: false,
+				},
+			},
+			name: {
+				type: String,
+				required: true,
+			},
+		},
+		sessionId: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		paymentStatus: {
+			type: String,
+			required: true,
+			enum: ['pending', 'complete', 'refunded'],
+			default: 'pending',
+		},
 		createdAt: {
 			type: Date,
 			default: Date.now,
 		},
 	},
-	{ timestamps: true }
+	{ timestamps: true, unique: false }
 )
 
 const Order = model<IOrderSchema>('Order', OrderSchema)
