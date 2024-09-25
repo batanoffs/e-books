9fef91b7f309b845d13ab2a8c514a6f0af48d62a
batanoffs <batanoff.s@protonmail.com>
2024-09-25 20:58:12 +0300

    updated data display and component design

--- client/src/pages/checkout/PaymentSuccessful.tsx
+++ client/src/pages/checkout/PaymentSuccessful.tsx
@@ -1,11 +1,11 @@
 import { useState, useEffect, useCallback } from 'react'
 import axios from 'axios'
 import API from '../../utils/constants/api'
-import useCartStore from '../../store/cart'
+import { Product } from '../../interfaces/product.interface'
+import { Box, Grid, Typography, List, ListItem, ListItemText, Divider } from '@mui/material'
 
 const PaymentSuccessful = () => {
 	const [orderData, setOrderData] = useState<any | null>(null)
-	const setCart = useCartStore((state) => state.setCart)
 
 	const fetchOrder = useCallback(async (sessionId: string | null) => {
 		if (!sessionId) return
@@ -17,7 +17,7 @@ const PaymentSuccessful = () => {
 				},
 			})
 
-			setOrderData(response.data.createdOrder)
+			setOrderData(response.data)
 		} catch (error) {
 			console.log(error)
 		}
@@ -27,37 +27,105 @@ const PaymentSuccessful = () => {
 		const queryString = window.location.search
 		const urlParams = new URLSearchParams(queryString)
 		const sessionId = urlParams.get('session_id')
-
-		fetchOrder(sessionId)
+		if (sessionId) {
+			fetchOrder(sessionId)
+		}
 	}, [fetchOrder])
 
+	const nameData = orderData?.shipping_details?.name || null
+	const name = nameData ? nameData[0]?.toUpperCase() + nameData.slice(1) : null
 	return (
-		<section id='success'>
-			{orderData && (
+		<Box
+			id='success'
+			sx={{
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+				justifyContent: 'center',
+				padding: '1rem',
+			}}
+		>
+			{orderData ? (
 				<>
-					<h1> Благодарим ви за Вашата поръчка! </h1>
-					<p>
-						Номер на поръчката: <strong>{orderData.id}</strong>
-					</p>
-					<ul>
-						{orderData?.products?.map((product) => (
-							<li key={product.id}>
-								<img src={product.picture} alt={product.title} />
-								<p>{product.title}</p>
-								<p>{product.price}</p>
-							</li>
+					<Typography variant='h3' gutterBottom align='center'>
+						{name ? `, ${name}` : ''}, Благодарим ви за Вашата поръчка!
+					</Typography>
+					<List>
+						<ListItem>
+							<ListItemText primary='Номер на поръчката:' secondary={orderData._id} />
+						</ListItem>
+						<ListItem>
+							<ListItemText
+								primary='Дата на поръчката:'
+								secondary={new Date(orderData.createdAt).toLocaleString()}
+							/>
+						</ListItem>
+						<ListItem>
+							<ListItemText
+								primary='Статус на поръчката:'
+								secondary={orderData.shippingStatus}
+							/>
+						</ListItem>
+						<ListItem>
+							<ListItemText
+								primary='Адрес:'
+								secondary={
+									orderData.shipping_details.address.city +
+									', ' +
+									orderData.shipping_details.address.country +
+									', ' +
+									orderData.shipping_details.address.postalCode
+								}
+							/>
+						</ListItem>
+						<Divider />
+						<ListItem>
+							<ListItemText primary='Продукти:' />
+						</ListItem>
+						{orderData?.products?.map((product: Product) => (
+							<ListItem key={product.productId._id}>
+								<Grid container spacing={1}>
+									<Grid item xs={12} sm={4}>
+										<img
+											src={product.productId.picture}
+											alt={product.productId.title}
+											style={{ width: '50px', height: 'auto' }}
+										/>
+									</Grid>
+									<Grid item xs={12} sm={8}>
+										<Typography variant='body2' gutterBottom>
+											{product.productId.title}
+										</Typography>
+										<Typography variant='body2' gutterBottom>
+											Количество: {product.quantity}
+										</Typography>
+										<Typography variant='body2' gutterBottom>
+											Цена: {product.productId.price} лв.
+										</Typography>
+									</Grid>
+								</Grid>
+							</ListItem>
 						))}
-					</ul>
-					<p>
-						Ние ценим вашите покупки! Потвърждаващ имейл ще ви бъде изпратен на. Ако имате
-						въпроси, моля, свържете се на имейл{' '}
-						<a href='mailto:orders@example.com'>orders@example.com</a>.
-					</p>
+					</List>
+					<Typography width='50%' variant='body1' gutterBottom align='center'>
+						Ние ценим вашите покупки! Потвърждаващ имейл ще ви бъде изпратен на{' '}
+						<b>{orderData.customer_details.email}</b>. Ако имате въпроси, моля, свържете
+						се на имейл .<a href='mailto:orders@example.com'>orders@example.com</a>.
+					</Typography>
+				</>
+			) : (
+				<>
+					<Typography variant='h3' gutterBottom align='center'>
+						Възникна грешка, моля опитайте отново!
+					</Typography>
+
+					<Typography variant='body1' gutterBottom align='center'>
+						Липсва информация за вашата поръчка.
+					</Typography>
 				</>
 			)}
-		</section>
+		</Box>
 	)
 }
 
 export default PaymentSuccessful
-
