20d7b1295f1a089edcc51e1d4127c7aa7fb52838
batanoffs <batanoff.s@protonmail.com>
2024-09-26 19:08:38 +0300

    clean up

--- client/src/middlewares/guards.tsx
+++ client/src/middlewares/guards.tsx
@@ -5,8 +5,7 @@ import AdminPage from '../pages/ReactAdmin/Admin'
 
 const AdminGuard = () => {
 	const token = document.cookie.replace(/(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/, '$1')
-
-	const isAdmin = checkIfUserIsAdmin() // Function to check if the user is an admin, you need to implement this
+	const isAdmin = checkIfUserIsAdmin()
 
 	if (!token || !isAdmin) {
 		return <Navigate to='/login' />
