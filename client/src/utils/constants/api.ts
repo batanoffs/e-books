a94aa3caf9e7f385a31bf40881c48ef6f80ecb75
batanoffs <batanoff.s@protonmail.com>
2024-09-26 11:55:59 +0300

    update route end point name for better clarity

--- client/src/utils/constants/api.ts
+++ client/src/utils/constants/api.ts
@@ -10,7 +10,7 @@ const API = {
 	CATEGORIES: baseURL + '/api/categories/',
 	ORDERS: baseURL + '/api/orders/',
 	USERS: baseURL + '/api/users/',
-	USER_ID: baseURL + '/api/users/user-id',
+	USER_ID: baseURL + '/api/users/verify-user',
 	CART: baseURL + '/api/cart/',
 	ADMIN: baseURL + '/api/admin/',
 	CHECKOUT: baseURL + '/api/checkout/',
--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -10,7 +10,7 @@ import {
 const router = Router()
 
 router.get('/', getUsers)
-router.get('/user-id', getUserByIdFromToken)
+router.get('/verify-user', getUserByIdFromToken)
 router.get('/:id', getUserById)
 router.put('/:id', updateUser)
 router.delete('/:id', deleteUser)
