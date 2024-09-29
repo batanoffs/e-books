4fcbe2fb3dfdfffdde419fbd5e288ac4a6c6dc38
batanoffs <batanoff.s@protonmail.com>
2024-09-29 15:05:07 +0300

    move to constants

---
+++ client/src/utils/constants/promoServices.ts
@@ -0,0 +1,23 @@
+interface Icon {
+	icon: string
+	text: string
+}
+
+export const promoServices: Icon[] = [
+	{
+		icon: '/delivery.png',
+		text: 'Бърза доставка',
+	},
+	{
+		icon: '/gift.png',
+		text: 'Безплатна доставка над 49,80 лв.',
+	},
+	{
+		icon: '/book.png',
+		text: 'Над 500 световни бранда',
+	},
+	{
+		icon: '/shop.png',
+		text: 'Купи онлайн, вземи от магазин',
+	},
+]
