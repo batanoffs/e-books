7508b79e5799685d12cf7f983562c85be9df6291
batanoffs <batanoff.s@protonmail.com>
2024-09-29 19:17:20 +0300

    add cookie Options

--- server/src/config/express.ts
+++ server/src/config/express.ts
@@ -4,11 +4,14 @@ import { cors } from '../middlewares/cors'
 
 const express = require('express')
 const cookieParser = require('cookie-parser')
-
+const cookieOptions = {
+	secure: true, // allows cookies to be sent over HTTPS
+	httpOnly: true, // prevents JavaScript from accessing cookies
+}
 function configExpress(app: Express): void {
-	app.use(cookieParser(process.env.JWT_SECRET))
+	app.use(cookieParser(process.env.JWT_SECRET, cookieOptions))
 	app.use(session())
-	app.use(cors());
+	app.use(cors())
 	app.use(express.json())
 }
 
