5790b31aade6c22c0c10b4fe7907955fdf2cfa0b
batanoffs <batanoff.s@protonmail.com>
2024-10-03 19:00:12 +0300

    add components

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,8 @@ import { ThemeProvider } from '@mui/material/styles'
 import themeOptions from './utils/helpers/theme'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+import { ScrollToTopAction } from './components/ScrollTop/BackToTop'
+import GlobalSpinner from './components/utils/Spinner'
 
 import App from './App'
 import './styles/index.scss'
@@ -18,6 +20,8 @@ root.render(
 		<ThemeProvider theme={themeOptions}>
 			<Router>
 				<App />
+				<ScrollToTopAction />
+				<GlobalSpinner />
 			</Router>
 		</ThemeProvider>
 	</React.StrictMode>
