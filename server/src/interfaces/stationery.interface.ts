dc277c2879fd921f336620e8912603f21b3990e5
batanoffs <batanoff.s@protonmail.com>
2024-10-03 19:01:45 +0300

    update types

--- server/src/interfaces/stationery.interface.ts
+++ server/src/interfaces/stationery.interface.ts
@@ -3,9 +3,11 @@ import { Types } from 'mongoose'
 export interface IStationerySchema {
 	title: string
 	price: number
-	description: string
+	description?: string
+	productType: string
 	picture: string
 	categories: Types.ObjectId[]
+	dimensions?: string
 	stock: number
 	createdAt: Date
 }
