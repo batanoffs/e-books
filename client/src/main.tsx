a7a347a213319c74299e6c4d74524783d1e4fda8
batanoffs <batanoff.s@protonmail.com>
2024-10-04 14:40:50 +0300

    move themeprovider to app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { ThemeProvider } from '@mui/material/styles'
-import themeOptions from './utils/helpers/theme'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 import { ScrollToTopAction } from './components/ScrollTop/BackToTop'
@@ -17,12 +15,10 @@ const container = document.getElementById('root')
 const root = createRoot(container!)
 root.render(
 	<React.StrictMode>
-		<ThemeProvider theme={themeOptions}>
 			<Router>
+				<GlobalSpinner />
 				<App />
 				<ScrollToTopAction />
-				<GlobalSpinner />
 			</Router>
-		</ThemeProvider>
 	</React.StrictMode>
 )
