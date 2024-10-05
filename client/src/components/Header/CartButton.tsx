1590ab0921c7eb27d5d7c3f814466daa98e79473
batanoffs <batanoff.s@protonmail.com>
2024-10-05 23:18:13 +0300

    update to support dropdown menu for current products in cart

--- client/src/components/Header/CartButton.tsx
+++ client/src/components/Header/CartButton.tsx
@@ -1,44 +1,92 @@
-import { useNavigate } from 'react-router-dom'
-import { useTheme } from '@mui/material/styles'
+import { MouseEvent, useState } from 'react'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
-import useMediaQuery from '@mui/material/useMediaQuery'
-// import Button from '@mui/material/Button'
-import Badge from '@mui/material/Badge'
+import { IconButton, Badge, Typography, Popper, Paper, Button, ListItem } from '@mui/material'
 
 import useCartStore from '../../store/cart'
-import { IconButton } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+import { ProductsTable } from '../Tables/ProductsTable'
 
-const CartButton = () => {
+export const CartDropdown = () => {
+	const [anchorEl, setAnchorEl] = useState(null)
+	const cartItems = useCartStore.getState().cart
+	const open = Boolean(anchorEl)
 	const navigate = useNavigate()
-	// const theme = useTheme()
-	// const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'))
+
+	//TODO update items display and fix issues
+	const totalItems = cartItems.reduce((sum, item) => sum + item.quantity, 0)
 
 	const handleNavigateToCart = () => {
 		navigate('/cart')
 	}
 
-	const cartItemCount = useCartStore((state) => state.cart.length)
+	const handleClick = (event: MouseEvent) => {
+		setAnchorEl(anchorEl ? null : event.currentTarget)
+	}
+
+	const handleClose = () => {
+		setAnchorEl(null)
+	}
 
 	return (
-		<Badge badgeContent={cartItemCount} color='secondary'>
+		<Badge badgeContent={cartItems.length} color='secondary'>
 			<IconButton
 				color='primary'
 				size={'large'}
-				sx={{ bgcolor: '#eee', '&:hover': { bgcolor: '#ccc' } }}
-				onClick={handleNavigateToCart}
+				sx={{
+					'&:hover': { bgcolor: '#eee' },
+					m: 0,
+					py: 0.7,
+					minWidth: '60px',
+					border: '1px solid darkgray',
+					borderRadius: 0.5,
+				}}
+				// onClick={handleNavigateToCart}
+				onClick={handleClick}
 			>
-				<ShoppingCartIcon />
+				<ShoppingCartIcon sx={{ color: 'darkgray' }} />
 			</IconButton>
 
-			{/* <Button
-				variant='contained'
-				color='primary'
-				size={'large'}
-				startIcon={<ShoppingCartIcon />}
-				onClick={handleNavigateToCart}
-			></Button> */}
+			<Popper
+				open={open}
+				anchorEl={anchorEl}
+				transition
+				disablePortal
+				placement='top-end'
+				style={{
+					height: 'auto',
+					width: '200px',
+					position: 'absolute',
+					top: 50,
+					zIndex: 5,
+				}}
+			>
+				<Paper
+					sx={{
+						padding: '0.5em',
+						borderRadius: '0.5em',
+						minWidth: '150px',
+						marginBottom: '0.5em',
+						visibility: 'visible',
+					}}
+				>
+					{' '}
+					<Typography variant='body2' sx={{ fontWeight: 'bold', textAlign: 'center' }}>
+						Количка
+					</Typography>
+					{cartItems.length > 0 ? (
+						<ProductsTable products={cartItems} />
+					) : (
+						<ListItem>Вашата количка е празна</ListItem>
+					)}
+					<Button
+						variant='contained'
+						sx={{ width: '100%' }}
+						onClick={handleNavigateToCart}
+					>
+						Към количката
+					</Button>
+				</Paper>
+			</Popper>
 		</Badge>
 	)
 }
-
-export default CartButton
