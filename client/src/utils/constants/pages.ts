f195c8ae319c37f523be83632d6f74b294c329fd
batanoffs <batanoff.s@protonmail.com>
2024-10-05 23:12:41 +0300

    add constants

--- client/src/utils/constants/pages.ts
+++ client/src/utils/constants/pages.ts
@@ -1,1 +1,8 @@
 export const navPages = ['Начало', 'Книги', 'Учебници', 'Канцелария', 'Най-продавани', 'Контакти']
+
+export const menuItems = [
+	{ name: 'Начало', path: '/' },
+	{ name: 'Профил', path: '/profile/settings' },
+	{ name: 'Харесани', path: '/profile/wishlist' },
+	{ name: 'Поръчки', path: '/profile/orders' },
+]
