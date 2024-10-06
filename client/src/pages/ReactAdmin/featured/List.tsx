7da4e84c1ca1aa8db0d810c480ab863c7f64edc0
batanoffs <batanoff.s@protonmail.com>
2024-10-06 15:41:58 +0300

    create index and export all featured components with named export

--- client/src/pages/ReactAdmin/featured/List.tsx
+++ client/src/pages/ReactAdmin/featured/List.tsx
@@ -1,18 +1,16 @@
-import { List, Datagrid, TextField, NumberField, ImageField } from 'react-admin';
+import { List, Datagrid, TextField, NumberField, ImageField } from 'react-admin'
 
-const FeaturedList = (props: any) => (
-    <List {...props}>
-        <Datagrid rowClick="edit">
-            <TextField source="id" label="ID" />
-            <TextField source="title" label="Заглавие" />
-            <TextField source="author" label="Автор" />
-            <NumberField source="price" label="Цена" />
-            <TextField source="description" label="Описание" />
-            <ImageField source="imageUrl" label="Изображение" />
-            <NumberField source="stock" label="Количество" />
-            <TextField source="category" label="Категория" />
-        </Datagrid>
-    </List>
-);
-
-export default FeaturedList;
+export const FeaturedList = (props: any) => (
+	<List {...props}>
+		<Datagrid rowClick='edit'>
+			<TextField source='id' label='ID' />
+			<TextField source='title' label='Заглавие' />
+			<TextField source='author' label='Автор' />
+			<NumberField source='price' label='Цена' />
+			<TextField source='description' label='Описание' />
+			<ImageField source='imageUrl' label='Изображение' />
+			<NumberField source='stock' label='Количество' />
+			<TextField source='category' label='Категория' />
+		</Datagrid>
+	</List>
+)
