60427aa7b498b1d8f5b66414cda3e816b9818322
batanoffs <batanoff.s@protonmail.com>
2024-10-06 13:09:31 +0300

    refactor props for consistency added more items

--- client/src/utils/constants/pages.ts
+++ client/src/utils/constants/pages.ts
@@ -1,8 +1,54 @@
-export const navPages = ['Начало', 'Книги', 'Учебници', 'Канцелария', 'Най-продавани', 'Контакти']
+export const navPages = [
+	{ title: 'Начало', href: '/' },
+	{ title: 'Книги', href: '/catalog/books/' },
+	{ title: 'Учебници', href: '/catalog/textbooks/' },
+	{ title: 'Канцелария', href: '/catalog/stationery/' },
+	{ title: 'Най-продавани', href: '/catalog/popular' },
+	{ title: 'Промотирани', href: '/catalog/promo' },
+	{ title: 'Контакти', href: '/contact' },
+]
+
+export const profileMenuItems = [
+	{ title: 'Профил', href: 'settings' },
+	{ title: 'Харесани', href: 'wishlist' },
+	{ title: 'Поръчки', href: 'orders' },
+]
 
 export const menuItems = [
-	{ name: 'Начало', path: '/' },
-	{ name: 'Профил', path: '/profile/settings' },
-	{ name: 'Харесани', path: '/profile/wishlist' },
-	{ name: 'Поръчки', path: '/profile/orders' },
+	{ title: 'Начало', path: '/' },
+	{ title: 'Профил', path: '/profile/settings' },
+	{ title: 'Харесани', path: '/profile/wishlist' },
+	{ title: 'Поръчки', path: '/profile/orders' },
+]
+
+export const footerCategoryLinks = [
+	{ title: 'Книжарница', href: '/knizharnitsa' },
+	{ title: 'За училище', href: '/za-uchilishte' },
+	{ title: 'Канцеларски материали', href: '/kantselarski-materiali' },
+	{ title: 'Хоби и настолни игри', href: '/hobi-i-nastolni-igri' },
+	{ title: 'Игри и играчки', href: '/igri-i-igrachki' },
+	{ title: 'Подаръци', href: '/gifts' },
+	{ title: 'Музика и филми', href: '/muzika-i-filmi' },
+]
+
+export const footerCompanyLinks = [
+	{ title: 'За нас', href: '/about' },
+	{ title: 'Цени и срокове за доставка', href: '/prices' },
+	{ title: 'Политика за поверителност', href: '/politics' },
+	{ title: 'Кариери', href: '/career' },
+	{ title: 'Връщане и замяна на стока', href: '/refund' },
+	{
+		title: 'Настройки за поверителност',
+		href: '/privacy-settings',
+		extraProps: { 'data-amcookie-js': 'footer-link' },
+	},
+	{ title: 'Реклама в книги бг', href: '/advertising' },
+	{ title: 'Онлайн решаване на спорове', href: '/support' },
+	{ title: 'Свържи се с нас', href: '/contact' },
+	{ title: 'Общи условия', href: '/terms' },
+	{ title: 'Често задавани въпроси', href: '/faq' },
+	{ title: 'Магазини книги бг center', href: '/shops' },
+	{ title: 'Начин на плащане', href: '/payment' },
+	{ title: 'Защита на лични данни', href: '/privacy' },
+	{ title: 'Франчайз', href: '/franchise' },
 ]
