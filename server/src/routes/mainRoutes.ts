b6c3f8d798f82957c401fd2552c85bdea4618ae4
batanoffs <batanoff.s@protonmail.com>
2024-10-09 02:41:41 +0300

    add stationery routes

--- server/src/routes/mainRoutes.ts
+++ server/src/routes/mainRoutes.ts
@@ -6,6 +6,7 @@ import userRoutes from './user'
 import adminRoutes from './admin'
 import authRoutes from './auth'
 import textbookRoutes from './textbook'
+import stationeryRoutes from './stationery'
 import cartRoutes from './cart'
 import featuredRoutes from './featured'
 import stripeRoutes from './stripe'
@@ -21,6 +22,7 @@ router.use('/orders', orderRoutes)
 router.use('/users', userRoutes)
 router.use('/admin', adminRoutes)
 router.use('/textbooks', textbookRoutes)
+router.use('/stationery', stationeryRoutes)
 router.use('/cart', cartRoutes)
 router.use('/featured', featuredRoutes)
 router.use('/checkout', stripeRoutes)
--- server/src/routes/stationery.ts
+++ server/src/routes/stationery.ts
@@ -1,1 +1,9 @@
-//TODO
+import { Router } from 'express'
+import { getStationeries, getStationeryById } from '../controllers/stationery'
+
+const router = Router()
+
+router.get('/', getStationeries)
+router.get('/:id', getStationeryById)
+
+export default router
