3095ea00625b73097a7be970385af362b4b1149c
batanoffs <batanoff.s@protonmail.com>
2024-10-10 02:50:09 +0300

    refactor helper function

--- client/src/utils/helpers/getNavigationParams.ts
+++ client/src/utils/helpers/getNavigationParams.ts
@@ -1,22 +1,14 @@
 import { Params } from 'react-router-dom'
 
-export const getNavigationParams = (params: Readonly<Params<string>>) => {
-	const [productType, productCategory] = Object.values(params)[0]?.split('/') ?? []
-	const headerText =
+export const getProductCategoryBG = (params: Readonly<Params<string>>) => {
+	const tokens = Object.values(params)[0]?.split('/') ?? []
+	const productType = tokens[0]
+	const productTypeBG =
 		{
 			books: 'книги',
 			textbooks: 'учебници',
 			stationery: 'канцелария',
 		}[productType] ?? productType
 
-	const shortNav = `книжарница / ${headerText}`
-	const longNav = `книжарница / ${headerText} / ${productCategory}`
-	const navString = productCategory ? longNav : shortNav
-	
-	return {
-		navString,
-		productCategory,
-		headerText,
-		productType,
-	}
+	return { productTypeBG, productType }
 }
