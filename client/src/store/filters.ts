b63d57cd5fc085b7d1273fb3091da6d1001e0517
batanoffs <batanoff.s@protonmail.com>
2024-10-10 02:50:53 +0300

    renamed props for clarity

--- client/src/store/filters.ts
+++ client/src/store/filters.ts
@@ -1,12 +1,12 @@
 import { create } from 'zustand'
 
 type CategoriesState = {
-	navCategory: string | null
+	productCategory: string | null
 	bookCategories: string[]
 	textbookCategories: string[]
 	stationeryCategories: string[]
 	filters: string[]
-	setNavCategory: (category: string | null) => void
+	setProductCategory: (category: string | null) => void
 	setFilters: (filters: string[]) => void
 	setBookCategories: (categories: string[]) => void
 	setTextbookCategories: (categories: string[]) => void
@@ -14,7 +14,7 @@ type CategoriesState = {
 }
 
 const useFiltersStore = create<CategoriesState>((set) => ({
-	navCategory: 'all',
+	productCategory: 'всички',
 	bookCategories: [],
 	textbookCategories: [],
 	stationeryCategories: [],
@@ -22,7 +22,7 @@ const useFiltersStore = create<CategoriesState>((set) => ({
 	setBookCategories: (bookCategories: string[]) => set({ bookCategories }),
 	setTextbookCategories: (textbookCategories: string[]) => set({ textbookCategories }),
 	setStationeryCategories: (stationeryCategories: string[]) => set({ stationeryCategories }),
-	setNavCategory: (navCategory: string | null) => set({ navCategory }),
+	setProductCategory: (navCategory: string | null) => set({ productCategory: navCategory }),
 	setFilters: (filters: string[]) => set({ filters }),
 }))
 
