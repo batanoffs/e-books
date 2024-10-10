8e8e54f81ddd5deb44f21223c46d31829ea4236c
batanoffs <batanoff.s@protonmail.com>
2024-10-10 13:31:14 +0300

    update order of middlewares and remove powered by express

--- server/src/config/express.ts
+++ server/src/config/express.ts
@@ -2,19 +2,19 @@ import express, { Express } from 'express'
 import { session } from '../middlewares/session'
 import { corsConfig } from '../middlewares/cors'
 
-const cookieParser = require('cookie-parser')
+var cookieParser = require('cookie-parser')
 const cookieOptions = {
 	secure: true, // allows cookies to be sent over HTTPS
 	httpOnly: true, // prevents JavaScript from accessing cookies
 }
 function configExpress(app: Express): void {
 	app.use(cookieParser(process.env.JWT_SECRET, cookieOptions))
-	app.use(session())
 	const { corsMiddleware, customMiddleware } = corsConfig()
-
+	app.set('X-Powered-By:', false)
 	// Apply them one after another
 	app.use(corsMiddleware) // Apply CORS middleware
 	app.use(customMiddleware) // Apply custom header middleware
+	app.use(session())
 	app.use(express.json())
 }
 
