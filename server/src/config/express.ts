4a29d53726f82cbbbb074f8b0121ef9daa537c53
batanoffs <batanoff.s@protonmail.com>
2024-10-10 14:35:52 +0300

    update on disable headers and sameSite prop to cookies

--- server/src/config/express.ts
+++ server/src/config/express.ts
@@ -3,14 +3,18 @@ import { session } from '../middlewares/session'
 import { corsConfig } from '../middlewares/cors'
 
 var cookieParser = require('cookie-parser')
+
 const cookieOptions = {
 	secure: true, // allows cookies to be sent over HTTPS
 	httpOnly: true, // prevents JavaScript from accessing cookies
+	sameSite: 'None', // allows cookies to be sent in cross-site requests
 }
+
 function configExpress(app: Express): void {
 	app.use(cookieParser(process.env.JWT_SECRET, cookieOptions))
 	const { corsMiddleware, customMiddleware } = corsConfig()
-	app.set('X-Powered-By:', false)
+	app.disable('x-powered-by')
+	app.disable('x-render-origin-server')
 	// Apply them one after another
 	app.use(corsMiddleware) // Apply CORS middleware
 	app.use(customMiddleware) // Apply custom header middleware
