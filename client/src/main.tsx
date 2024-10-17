6fa3f870c64c40de1dc49bcc754ae3daa6567ffd
batanoffs <batanoff.s@protonmail.com>
2024-10-17 12:54:46 +0300

    add helmet provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 import { ScrollToTopAction, GlobalSpinner } from './components/index'
+import { HelmetProvider } from 'react-helmet-async'
 
 import App from './App'
 import './styles/index.scss'
@@ -14,10 +15,12 @@ const container = document.getElementById('root')
 const root = createRoot(container!)
 root.render(
 	<React.StrictMode>
-		<Router>
-			<GlobalSpinner />
-			<App />
-			<ScrollToTopAction />
-		</Router>
+		<HelmetProvider>
+			<Router>
+				<GlobalSpinner />
+				<App />
+				<ScrollToTopAction />
+			</Router>
+		</HelmetProvider>
 	</React.StrictMode>
 )
