5cbc21c320008f9fd74b240858febe020f556b45
batanoffs <batanoff.s@protonmail.com>
2025-02-27 15:25:54 +0200

    add SnackbarProvider for toast notifications and improve component structure in App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,17 @@
 import { Route, Routes, useLocation } from 'react-router-dom'
 import { useEffect, useState, useMemo } from 'react'
 import { ThemeProvider, ThemeWithProps, createTheme } from '@mui/material/styles'
-import CssBaseline from '@mui/material/CssBaseline'
+import { SnackbarProvider } from 'notistack'
 
+import { HomePage, ContactsPage, FaqPage, LoginModal, CartPage, Register, AdminPage, CheckoutPage, ProfilePage, PaymentSuccessful, CatalogPage, PopularPage, AboutPage } from './pages'
+import { Header, Footer, PageNotFound, ScrollTopButton } from './components'
 import { useThemeSettings } from './store/themeSettings'
 import { themeOptions } from './utils/helpers/theme'
-import { Header, Footer, PageNotFound, ScrollTopButton } from './components'
-import {
-	HomePage,
-	ContactsPage,
-	FaqPage,
-	LoginModal,
-	CartPage,
-	Register,
-	AdminPage,
-	CheckoutPage,
-	ProfilePage,
-	PaymentSuccessful,
-	CatalogPage,
-	PopularPage,
-	AboutPage,
-} from './pages'
 
+import CssBaseline from '@mui/material/CssBaseline'
+        
 const App = () => {
-	const [isAdmin, setIsAdmin] = useState(false)
+	const [isAdmin, setIsAdmin] = useState<boolean>(false)
 	const { darkMode, toggleDarkMode } = useThemeSettings()
 	let location = useLocation()
 
@@ -39,27 +27,28 @@ const App = () => {
 
 	return (
 		<ThemeProvider theme={myTheme}>
-			{!isAdmin && <Header />}
-			{!isAdmin && <ScrollTopButton />}
-			<LoginModal />
-			<CssBaseline />
-			<Routes>
-				<Route path='/' element={<HomePage />} />
-				<Route path='/catalog/*' element={<CatalogPage />} />
-				<Route path='/admin/*' element={<AdminPage />} />
-				<Route path='/cart' element={<CartPage />} />
-				<Route path='/checkout' element={<CheckoutPage />} />
-				<Route path='/success' element={<PaymentSuccessful />} />
-				<Route path='/contacts' element={<ContactsPage />} />
-				<Route path='/popular' element={<PopularPage />} />
-				<Route path='/about' element={<AboutPage />} />
-				<Route path='/faq' element={<FaqPage />} />
-				<Route path='/register' element={<Register />} />
-				<Route path='*' element={<PageNotFound />} />
-				<Route path='/profile/*' element={<ProfilePage />} />
-			</Routes>
-
-			{!isAdmin && <Footer />}
+			<SnackbarProvider maxSnack={3}>
+				{!isAdmin && <Header />}
+				{!isAdmin && <ScrollTopButton />}
+				<LoginModal />
+				<CssBaseline />
+				<Routes>
+					<Route path='/' element={<HomePage />} />
+					<Route path='/catalog/*' element={<CatalogPage />} />
+					<Route path='/admin/*' element={<AdminPage />} />
+					<Route path='/cart' element={<CartPage />} />
+					<Route path='/checkout' element={<CheckoutPage />} />
+					<Route path='/success' element={<PaymentSuccessful />} />
+					<Route path='/contacts' element={<ContactsPage />} />
+					<Route path='/popular' element={<PopularPage />} />
+					<Route path='/about' element={<AboutPage />} />
+					<Route path='/faq' element={<FaqPage />} />
+					<Route path='/register' element={<Register />} />
+					<Route path='*' element={<PageNotFound />} />
+					<Route path='/profile/*' element={<ProfilePage />} />
+				</Routes>
+				{!isAdmin && <Footer />}
+			</SnackbarProvider>
 		</ThemeProvider>
 	)
 }
