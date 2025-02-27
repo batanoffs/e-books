05455e14d16ef8f17d588f3b3a6afdc43766899b
batanoffs <batanoff.s@protonmail.com>
2025-02-27 15:20:16 +0200

    update footer margins and layout

--- client/src/components/Footer/bottom/FooterBot.tsx
+++ client/src/components/Footer/bottom/FooterBot.tsx
@@ -3,31 +3,25 @@ import styles from './footer-bottom.module.scss'
 export const FooterBot = () => {
 	return (
 		<div className={styles.pageFooterBot}>
-			<div className={styles.wrapper}>
-				<div className={styles.pageFooterBotBlock}>
-					<div className={`${styles.pageFooterBotItem} ${styles.pageFooterBotItemLarge}`}>
-						<span className={styles.copyright}>
-							Книжарница „книги бг“ е запазена марка на книжарница "книги бг АД", BG
-						</span>
-						<span className={styles.copyright}>
-							2024 © книги бг. Всички права запазени.
-						</span>
-					</div>
-					<div className={styles.pageFooterBotItem}>
-						<span className={styles.copyright}>
-							<span>Дизайн и програмиране от </span>
-							<a
-								href='https://danielbatanov.netlify.app/'
-								title='Batanoffs'
-								target='_blank'
-								rel='noopener noreferrer'
-							>
-								<img width='59' height='12' src='' alt='Batanoffs' />
-								<span className={styles.visuallyHidden}>Batanoffs</span>
-							</a>
-						</span>
-					</div>
-				</div>
+			<div className={styles.pageFooterBotItemLarge}>
+				<span className={styles.copyright}>
+					Книжарница „книги бг“ е запазена марка на книжарница "книги бг АД", BG 2024 ©
+					книги бг. Всички права запазени.
+				</span>
+			</div>
+			<div className={styles.pageFooterBotItem}>
+				<span className={styles.copyright}>
+					<span>Дизайн и програмиране</span>
+					<a
+						href='https://daniel-batanov.onrender.com'
+						title='Batanoffs'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						<img width='59' height='12' src='' alt='batanoffs' />
+						<span className={styles.visuallyHidden}>batanoffs</span>
+					</a>
+				</span>
 			</div>
 		</div>
 	)
