fe8b44a9982b1b45425040bc4f8dfa63ac89bd01
batanoffs <batanoff.s@protonmail.com>
2025-02-27 15:21:23 +0200

    add demo users login buttons for easy access and authentication. add constants with credentials

--- client/src/pages/login/LoginModal.tsx
+++ client/src/pages/login/LoginModal.tsx
@@ -10,31 +10,59 @@ import Paper from '@mui/material/Paper'
 
 import { useLoginModal } from '../../store/helperModal'
 import authService from '../../services/authService'
-import useAlertStore from '../../store/alert'
+import { useAlert } from '../../hooks/useAlert'
+import DEMO_USERS from './demoUsers'
 
 import styles from './login-page.module.scss'
 
 export const LoginModal = () => {
+	// Define form state
 	const [email, setEmail] = useState('')
 	const [password, setPassword] = useState('')
+
+	// Get hooks to manage modal alert and navigation
 	const open = useLoginModal((state) => state.open)
 	const toggleOpen = useLoginModal((state) => state.toggleOpen)
-	const showAlert = useAlertStore((state) => state.showAlert)
+	
+	const { showAlert } = useAlert()
 	const navigate = useNavigate()
 
+	// Define handlers
 	const handleLogin = async (e: React.FormEvent) => {
 		e.preventDefault()
 		try {
 			const response = await authService.login(email, password)
 			if (!response) throw new Error('Response data is empty')
+
 			const { redirectUrl, message } = response
+
 			toggleOpen()
 			navigate(redirectUrl)
 			showAlert(message, 'success')
+			setEmail('')
+			setPassword('')
 		} catch (error) {
 			showAlert('Невалиден имейл или парола', 'error')
 		}
 	}
+
+	const handleDemoUsersLogin = async (demoEmail: string, demoPass: string) => {
+		try {
+			const response = await authService.login(demoEmail, demoPass)
+			if (!response) throw new Error('Response data is empty')
+
+			const { redirectUrl, message } = response
+
+			toggleOpen()
+			navigate(redirectUrl)
+			showAlert(message, 'success')
+			setEmail('')
+			setPassword('')
+		} catch (error) {
+			showAlert('Невалиден имейл или парола', 'error')
+		}
+	}
+
 	const handleRegisterRedirect = (e: React.FormEvent) => {
 		e.preventDefault()
 		navigate('/register')
@@ -74,13 +102,28 @@ export const LoginModal = () => {
 						onChange={(e) => setPassword(e.target.value)}
 						required
 					/>
+					<Button type='submit' variant='contained' color='primary'>
+						Вход
+					</Button>
 					<Button
 						type='submit'
 						variant='contained'
 						color='primary'
-						className={styles.button}
+						onClick={(e) =>
+							handleDemoUsersLogin(DEMO_USERS.PESHO.EMAIL, DEMO_USERS.PESHO.PASS)
+						}
 					>
-						Вход
+						Login Demo User 1
+					</Button>
+					<Button
+						type='submit'
+						variant='contained'
+						color='primary'
+						onClick={(e) =>
+							handleDemoUsersLogin(DEMO_USERS.IVO.EMAIL, DEMO_USERS.IVO.PASS)
+						}
+					>
+						Login Demo User 2
 					</Button>
 					<Link
 						onClick={handleRegisterRedirect}
---
+++ client/src/pages/login/demoUsers.ts
@@ -0,0 +1,16 @@
+const PESHO = {
+	EMAIL: 'pesho@abv.bg',
+	PASS: 'qweasd123',
+}
+
+const IVO = {
+	EMAIL: 'ivo@abv.bg',
+	PASS: 'qweasd',
+}
+
+const DEMO_USERS = {
+	PESHO,
+	IVO,
+}
+
+export default DEMO_USERS;
